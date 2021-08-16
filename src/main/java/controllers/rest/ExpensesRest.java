a7e3f7ce387694ef1b8db9f677f87b4f6cc7a57d
VasGorn <vas.goroshko@gmail.com>
2021-08-16 20:26:45 +0300

    feat: add get the all expenses endpoint

--- src/main/java/controllers/rest/ExpensesRest.java
+++ src/main/java/controllers/rest/ExpensesRest.java
@@ -1,5 +1,23 @@
 package controllers.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import domain.Expenses;
+
+@RestController
+@RequestMapping(path = "/rest/expenses", produces = "application/json")
 public class ExpensesRest {
+	@Autowired
+	private ExpensesService expensesService;
+	
+	@GetMapping("/all")
+	public List<Expenses> expensesGetAll(){
+		return expensesService.getAll();
+	}
 
 }
