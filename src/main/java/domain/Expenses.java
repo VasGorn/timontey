819e6228376217ac6268cb4b8c84cb0ab4c5eed1
VasGorn <vas.goroshko@gmail.com>
2021-08-16 20:16:02 +0300

    feat: create expenses model

---
+++ src/main/java/domain/Expenses.java
@@ -0,0 +1,32 @@
+package domain;
+
+public class Expenses {
+	private long id;
+	private String name;
+	
+	public Expenses(long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Expenses() {
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Expenses [id=" + id + ", name=" + name + "]";
+	}
+
+}
