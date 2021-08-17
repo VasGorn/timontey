d79418f5557c0b3e8c0bffce60b13ad299d27733
VasGorn <vas.goroshko@gmail.com>
2021-08-17 19:52:39 +0300

    feat: add rest api endpoint for the delete expenses

--- src/main/java/controllers/rest/ExpensesRest.java
+++ src/main/java/controllers/rest/ExpensesRest.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -57,6 +58,12 @@ public class ExpensesRest {
 			expenses.setName(patch.getName());
 		}
 		return expensesService.update(expenses);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteExpenses(@PathVariable("id") Long id) {
+		expensesService.delete(id);
 	}
 
 }
