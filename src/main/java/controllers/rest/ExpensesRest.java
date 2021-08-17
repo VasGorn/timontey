f10b2a8a7f4687772c85f7830ffc43b83669fa56
VasGorn <vas.goroshko@gmail.com>
2021-08-17 19:34:13 +0300

    feat: add rest api endpoint for the put expenses

--- src/main/java/controllers/rest/ExpensesRest.java
+++ src/main/java/controllers/rest/ExpensesRest.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -41,6 +42,11 @@ public class ExpensesRest {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Expenses postExpenses(@RequestBody Expenses expenses) {
 		return expensesService.save(expenses);
+	}
+	
+	@PutMapping("/{id}")
+	public Expenses putExpenses(@RequestBody Expenses expenses) {
+		return expensesService.update(expenses);
 	}
 
 }
