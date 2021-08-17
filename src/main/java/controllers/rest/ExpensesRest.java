07b1ef29511f68707ad59ea8a6437a3971a1569a
VasGorn <vas.goroshko@gmail.com>
2021-08-17 19:18:38 +0300

    feat: add rest api endpoint for the get expenses by id

--- src/main/java/controllers/rest/ExpensesRest.java
+++ src/main/java/controllers/rest/ExpensesRest.java
@@ -1,9 +1,13 @@
 package controllers.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +19,16 @@ public class ExpensesRest {
 	@Autowired
 	private ExpensesService expensesService;
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Expenses> expensesById(@PathVariable("id") Long id) {
+		Optional<Expenses> optExpenses = expensesService.getExpensesById(id);
+		System.out.println("Expenses find: " + optExpenses);
+		if(optExpenses.isPresent()) {
+			return new ResponseEntity<Expenses>(optExpenses.get(), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	}
+
 	@GetMapping("/all")
 	public List<Expenses> expensesGetAll(){
 		return expensesService.getAll();
