0e59325430224629fbac0fdafe01d8a4039c6c01
VasGorn <vas.goroshko@gmail.com>
2021-08-18 19:18:02 +0300

    feat: add new methods to expenses service for get all, save, update, get by id and delete

--- src/main/java/service/ExpensesService.java
+++ src/main/java/service/ExpensesService.java
@@ -1,5 +1,20 @@
 package service;
 
+import java.util.List;
+import java.util.Optional;
+
+import domain.Expenses;
+
 public interface ExpensesService {
+
+	List<Expenses> getAll();
+
+	Expenses save(Expenses expenses);
+
+	Expenses update(Expenses expenses);
+
+	Optional<Expenses> getExpensesById(Long id);
+
+	void delete(Long id);
 
 }
