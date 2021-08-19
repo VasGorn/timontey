4c11fddc244be926aca0bf5e8257f463573837d1
VasGorn <vas.goroshko@gmail.com>
2021-08-19 19:45:50 +0300

    feat: create the expenses service implementation

---
+++ src/main/java/service/imp/ExpensesServiceImp.java
@@ -0,0 +1,41 @@
+package service.imp;
+
+import java.util.List;
+import java.util.Optional;
+
+import domain.Expenses;
+import service.ExpensesService;
+
+public class ExpensesServiceImp implements ExpensesService {
+
+	@Override
+	public List<Expenses> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Expenses save(Expenses expenses) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Expenses update(Expenses expenses) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Expenses> getExpensesById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
