3e74684e09ea92de105dd26ee406f7b4967c9908
VasGorn <vas.goroshko@gmail.com>
2021-08-20 20:16:34 +0300

    feat: implement methods of repository interface

--- src/main/java/repository/imp/ExpensesJdbcRepo.java
+++ src/main/java/repository/imp/ExpensesJdbcRepo.java
@@ -1,48 +1,84 @@
 package repository.imp;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import domain.Expenses;
 import repository.ExpensesRepository;
+import rowmapper.ExpensesRowMapper;
 
 @Repository
 public class ExpensesJdbcRepo implements ExpensesRepository {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
+	private static final String SELECT_ALL_SQL	= "SELECT * FROM expenses;";
+	private static final String INSERT_SQL		= "INSERT INTO expenses (name) VALUES "
+												   + "(?);";
+	private static final String UPDATE_SQL		= "UPDATE expenses SET name=? "
+												   + "WHERE id=?;";
+	private static final String DELETE_SQL		= "DELETE FROM expenses WHERE id=?;";
+	private static final String SELECT_ONE_SQL	= "SELECT * FROM expenses WHERE id=?;";
+
 	@Override
 	public List<Expenses> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Expenses> list = (List<Expenses>) jdbcTemplate.query(SELECT_ALL_SQL,
+				new ExpensesRowMapper());
+		System.out.println(Arrays.toString(list.toArray()));
+		return list;
+	
 	}
 
 	@Override
 	public long save(Expenses expenses) {
-		// TODO Auto-generated method stub
-		return 0;
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+
+	    jdbcTemplate.update(connection -> {
+	        PreparedStatement ps = connection
+	          .prepareStatement(INSERT_SQL, Statement.RETURN_GENERATED_KEYS);
+	          ps.setString(1, expenses.getName());
+	          return ps;
+	        }, keyHolder);
+	    
+	    long newId;
+	    
+	    if (keyHolder.getKeys().size() > 1) {
+	        newId = ((Number) keyHolder.getKeys().get("id")).longValue();
+	    } else {
+	        newId= keyHolder.getKey().longValue();
+	    }
+
+        return newId;
 	}
 
 	@Override
 	public int update(Expenses expenses) {
-		// TODO Auto-generated method stub
-		return 0;
+		return jdbcTemplate.update(UPDATE_SQL, expenses.getName(), expenses.getId());
 	}
 
 	@Override
 	public Optional<Expenses> findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return Optional.of(jdbcTemplate.queryForObject(SELECT_ONE_SQL, 
+				new ExpensesRowMapper(), id));
+		} catch (EmptyResultDataAccessException ex){
+			return Optional.empty();
+		}
 	}
 
 	@Override
-	public void delete(Long id) {
-		// TODO Auto-generated method stub
-
+	public int delete(Long id) {
+		return jdbcTemplate.update(DELETE_SQL, id);
 	}
 
 }
