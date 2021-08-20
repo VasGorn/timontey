d369690f9972e24f23bfc65d51c8a1088b10199f
VasGorn <vas.goroshko@gmail.com>
2021-08-20 19:11:05 +0300

    feat: add the inteface methods

--- src/main/java/repository/ExpensesRepository.java
+++ src/main/java/repository/ExpensesRepository.java
@@ -1,5 +1,20 @@
 package repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import domain.Expenses;
+
 public interface ExpensesRepository {
+
+	List<Expenses> getAll();
+
+	long save(Expenses expenses);
+
+	int update(Expenses expenses);
+
+	Optional<Expenses> findById(Long id);
+
+	void delete(Long id);
 
 }
