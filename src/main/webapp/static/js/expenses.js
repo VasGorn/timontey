2e3c8e7b784ba713656ef0d5fa6dd07ddd01edd1
VasGorn <vas.goroshko@gmail.com>
2021-08-24 19:42:43 +0300

    feat: get data from form

--- src/main/webapp/static/js/expenses.js
+++ src/main/webapp/static/js/expenses.js
@@ -39,3 +39,14 @@ function isInTable(name) {
 	});
 	return isExist;
 }
+
+function getFormData($form) {
+	var unindexed_array = $form.serializeArray();
+	var indexed_array = {};
+
+	$.map(unindexed_array, function(n, i) {
+		indexed_array[n['name']] = n['value'];
+	});
+
+	return indexed_array;
+}
