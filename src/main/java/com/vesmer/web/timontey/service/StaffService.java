bf5ca3db2c58300778085d33727c5ded381ea0fa
VasGorn <vas.goroshko@gmail.com>
2021-09-03 10:22:19 +0300

    feat: add methods to interface

--- src/main/java/com/vesmer/web/timontey/service/StaffService.java
+++ src/main/java/com/vesmer/web/timontey/service/StaffService.java
@@ -1,5 +1,20 @@
 package com.vesmer.web.timontey.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.vesmer.web.timontey.domain.Employee;
+
 public interface StaffService {
+
+	List<Employee> getAll();
+
+	Employee save(Employee emloyee);
+
+	Employee update(Employee employee);
+
+	void delete(long id);
+
+	Optional<Employee> getEmployeeById(long id);
 
 }
