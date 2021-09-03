40d953f9cee693ee28ee182482a7da92cec3b870
VasGorn <vas.goroshko@gmail.com>
2021-09-03 09:37:44 +0300

    feat: add endpoint to get all employees

--- src/main/java/com/vesmer/web/timontey/controllers/rest/StaffRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/StaffRest.java
@@ -1,13 +1,23 @@
 package com.vesmer.web.timontey.controllers.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vesmer.web.timontey.domain.Employee;
 
 @RestController
 @RequestMapping(path = "/rest/staff", produces = "application/json")
 public class StaffRest {
 	@Autowired
-	private StaffService expensesService;
+	private StaffService staffService;
+	
+	@GetMapping("/all")
+	public List<Employee> staffGetAll(){
+		return staffService.getAll();
+	}
 
 }
