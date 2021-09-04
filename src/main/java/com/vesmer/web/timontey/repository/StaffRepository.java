0974f48ac101ef74f2bdfcc2dca73525f5351d94
VasGorn <vas.goroshko@gmail.com>
2021-09-04 19:52:43 +0300

    feat: create staff repository interface

---
+++ src/main/java/com/vesmer/web/timontey/repository/StaffRepository.java
@@ -0,0 +1,20 @@
+package com.vesmer.web.timontey.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.vesmer.web.timontey.domain.Employee;
+
+public interface StaffRepository {
+
+	List<Employee> getAll();
+
+	long save(Employee employee);
+
+	int update(Employee employee);
+
+	int delete(long id);
+
+	Optional<Employee> findById(long id);
+
+}
