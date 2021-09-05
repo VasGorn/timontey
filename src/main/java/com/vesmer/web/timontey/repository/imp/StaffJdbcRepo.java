3a98921ca4625a230aee65971e35bfd9bcde63a0
VasGorn <vas.goroshko@gmail.com>
2021-09-05 20:17:19 +0300

    feat: create the staff jdbc repository implementation

---
+++ src/main/java/com/vesmer/web/timontey/repository/imp/StaffJdbcRepo.java
@@ -0,0 +1,87 @@
+package com.vesmer.web.timontey.repository.imp;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import com.vesmer.web.timontey.domain.Employee;
+import com.vesmer.web.timontey.repository.StaffRepository;
+import com.vesmer.web.timontey.rowmapper.EmployeeRowMapper;
+
+@Repository
+public class StaffJdbcRepo implements StaffRepository {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	private static final String SELECT_ALL_SQL	= "SELECT * FROM staff;";
+	private static final String INSERT_SQL		= "INSERT INTO staff (last_name, first_name, middle_name) "
+												   + "VALUES (?, ?, ?);";
+	private static final String UPDATE_SQL		= "UPDATE staff SET last_name=?, first_name=?, middle_name=? "
+												   + "WHERE id=?;";
+	private static final String DELETE_SQL		= "DELETE FROM staff WHERE id=?;";
+	private static final String SELECT_ONE_SQL	= "SELECT * FROM staff WHERE id=?;";
+	
+
+	@Override
+	public List<Employee> getAll() {
+		List<Employee> list = (List<Employee>) jdbcTemplate.query(SELECT_ALL_SQL,
+				new EmployeeRowMapper());
+		System.out.println(Arrays.toString(list.toArray()));
+		return list;
+	}
+
+	@Override
+	public long save(Employee employee) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+
+	    jdbcTemplate.update(connection -> {
+	        PreparedStatement ps = connection
+	          .prepareStatement(INSERT_SQL, Statement.RETURN_GENERATED_KEYS);
+	          ps.setString(1, employee.getLastName());
+	          ps.setString(2, employee.getFirstName());
+	          ps.setString(3, employee.getMiddleName());
+	          return ps;
+	        }, keyHolder);
+	    
+	    long newId;
+	    
+	    if (keyHolder.getKeys().size() > 1) {
+	        newId = ((Number) keyHolder.getKeys().get("id")).longValue();
+	    } else {
+	        newId= keyHolder.getKey().longValue();
+	    }
+
+        return newId;
+	}
+
+	@Override
+	public int update(Employee employee) {
+		return jdbcTemplate.update(UPDATE_SQL, employee.getLastName(), 
+				employee.getFirstName(), employee.getMiddleName(), employee.getId());
+	}
+
+	@Override
+	public int delete(long id) {
+		return jdbcTemplate.update(DELETE_SQL, id);
+	}
+
+	@Override
+	public Optional<Employee> findById(long id) {
+		try {
+			return Optional.of(jdbcTemplate.queryForObject(SELECT_ONE_SQL, 
+				new EmployeeRowMapper(), id));
+		} catch (EmptyResultDataAccessException ex){
+			return Optional.empty();
+		}
+	}
+
+}
