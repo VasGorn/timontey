3e2d7228a5ee3f2bc9bdb7bbe85b29b81d0cd5a5
VasGorn <vas.goroshko@gmail.com>
2021-09-06 20:28:03 +0300

    feat: add post rest api endpoint

--- src/main/java/com/vesmer/web/timontey/controllers/rest/StaffRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/StaffRest.java
@@ -3,8 +3,12 @@ package com.vesmer.web.timontey.controllers.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vesmer.web.timontey.domain.Employee;
@@ -19,6 +23,12 @@ public class StaffRest {
 	@GetMapping("/all")
 	public List<Employee> staffGetAll(){
 		return staffService.getAll();
+	}
+	
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Employee postEmployee(@RequestBody Employee employee) {
+		return staffService.save(employee);
 	}
 
 }
