a951fa0145210fe7f141920c028e4fd618777dd5
VasGorn <vas.goroshko@gmail.com>
2021-09-07 20:20:53 +0300

    feat: create variables for staff javascript

--- src/main/webapp/static/js/staff.js
+++ src/main/webapp/static/js/staff.js
@@ -1,1 +1,12 @@
-console.log("TODO: client logic to staff page");
+var btnAdd = document.getElementById("add");
+var btnUpdate = document.getElementById("update");
+var btnDelete = document.getElementById("delete");
+
+var txtLastName = document.getElementById("last_name");
+var txtFirstName = document.getElementById("first_name");
+var txtMiddleName = document.getElementById("middle_name");
+
+var URL_REST = "/timontey/rest/staff/";
+
+var $table = $('#table');
+var checkedRows = [];
