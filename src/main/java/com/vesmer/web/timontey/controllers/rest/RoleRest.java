4d354206375b8351cfaf1845e0c2850ae10010b4
VasGorn <vas.goroshko@gmail.com>
2021-09-15 19:23:30 +0300

    feat: create get all roles endpoint

--- src/main/java/com/vesmer/web/timontey/controllers/rest/RoleRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/RoleRest.java
@@ -1,10 +1,24 @@
 package com.vesmer.web.timontey.controllers.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vesmer.web.timontey.domain.Role;
 
 @RestController
 @RequestMapping(path="/rest/role", produces="application/json")
 public class RoleRest {
+	@Autowired
+	private RoleService roleService;
+	
+	@GetMapping("/all")
+	public List<Role> roleGetAll(){
+		return roleService.getAll();
+	}
+
 
 }
