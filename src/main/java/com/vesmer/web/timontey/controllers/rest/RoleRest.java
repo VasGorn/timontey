3208054805c6362d7faf6f2723fd08f84313463e
VasGorn <vas.goroshko@gmail.com>
2021-09-15 19:19:37 +0300

    feat: create class for role rest api

---
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/RoleRest.java
@@ -0,0 +1,10 @@
+package com.vesmer.web.timontey.controllers.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/rest/role", produces="application/json")
+public class RoleRest {
+
+}
