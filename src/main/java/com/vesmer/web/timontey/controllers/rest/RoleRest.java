5d1ed6cd608011720a1625efc1142e9a58ddad6b
VasGorn <vas.goroshko@gmail.com>
2021-09-16 19:31:12 +0300

    feat: create role service interface

--- src/main/java/com/vesmer/web/timontey/controllers/rest/RoleRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/RoleRest.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vesmer.web.timontey.domain.Role;
+import com.vesmer.web.timontey.service.RoleService;
 
 @RestController
 @RequestMapping(path="/rest/role", produces="application/json")
---
+++ src/main/java/com/vesmer/web/timontey/service/RoleService.java
@@ -0,0 +1,11 @@
+package com.vesmer.web.timontey.service;
+
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.Role;
+
+public interface RoleService {
+
+	List<Role> getAll();
+
+}
