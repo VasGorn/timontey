74d9c1877537f66fc9e7a1e2a7e381bc859a8156
VasGorn <vas.goroshko@gmail.com>
2021-09-16 19:47:20 +0300

    feat: create role repository interface

---
+++ src/main/java/com/vesmer/web/timontey/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.vesmer.web.timontey.repository;
+
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.Role;
+
+public interface RoleRepository {
+
+	List<Role> getAll();
+
+}
