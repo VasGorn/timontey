1928b28ca79d3a634fec9f68a7fbbc9b7a1b6e5a
VasGorn <vas.goroshko@gmail.com>
2021-09-18 20:06:30 +0300

    feat: add endpoint for get list of work types selected by role

--- src/main/java/com/vesmer/web/timontey/controllers/rest/WorkTypeRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/WorkTypeRest.java
@@ -1,13 +1,24 @@
 package com.vesmer.web.timontey.controllers.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vesmer.web.timontey.domain.WorkType;
 
 @RestController
 @RequestMapping(path = "/rest/work-type", produces = "application/json")
 public class WorkTypeRest {
 	@Autowired
 	private WorkTypeService workTypeService;
+	
+	@GetMapping(path = "/role/{id}")
+	public List<WorkType> getWorkTypesByRole(@PathVariable("id") long roleId){
+		return workTypeService.getWorkTypesByRole(roleId);
+	}
 
 }
