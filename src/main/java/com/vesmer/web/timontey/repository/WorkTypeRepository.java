9353d3ce262f9ff663005da18bc4a6738fa18292
VasGorn <vas.goroshko@gmail.com>
2021-09-19 20:39:16 +0300

    feat: add get list of work types by role method to repository interface

--- src/main/java/com/vesmer/web/timontey/repository/WorkTypeRepository.java
+++ src/main/java/com/vesmer/web/timontey/repository/WorkTypeRepository.java
@@ -1,5 +1,11 @@
 package com.vesmer.web.timontey.repository;
 
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.WorkType;
+
 public interface WorkTypeRepository {
+
+	List<WorkType> getWorkTypesByRole(long roleId);
 
 }
