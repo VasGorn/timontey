7a1b654cc5ceedbf03c6b0852576e7bc986d0560
VasGorn <vas.goroshko@gmail.com>
2021-09-19 20:27:39 +0300

    feat: create work type service implementation

---
+++ src/main/java/com/vesmer/web/timontey/service/imp/WorkTypeServiceImp.java
@@ -0,0 +1,18 @@
+package com.vesmer.web.timontey.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vesmer.web.timontey.domain.WorkType;
+import com.vesmer.web.timontey.service.WorkTypeService;
+
+@Service
+@Transactional
+public class WorkTypeServiceImp implements WorkTypeService {
+	@Autowired
+	private WorkTypeRepository workTypeRepository;
+
+}
