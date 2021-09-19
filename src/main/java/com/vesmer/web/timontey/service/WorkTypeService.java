b576af8c01563a5919b560ff865d135443cca3db
VasGorn <vas.goroshko@gmail.com>
2021-09-19 20:23:40 +0300

    feat: add method to get list of work types by role

--- src/main/java/com/vesmer/web/timontey/service/WorkTypeService.java
+++ src/main/java/com/vesmer/web/timontey/service/WorkTypeService.java
@@ -1,5 +1,11 @@
 package com.vesmer.web.timontey.service;
 
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.WorkType;
+
 public interface WorkTypeService {
+
+	List<WorkType> getWorkTypesByRole(long roleId);
 
 }
