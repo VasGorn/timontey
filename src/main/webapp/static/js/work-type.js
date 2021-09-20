7dfc1e0bd3ae577d5ce0fbaad34352c3cd9b8325
VasGorn <vas.goroshko@gmail.com>
2021-09-20 20:19:28 +0300

    feat: declaration of global variables

--- src/main/webapp/static/js/work-type.js
+++ src/main/webapp/static/js/work-type.js
@@ -1,1 +1,14 @@
-console.log("TODO")
+var btnAdd = document.getElementById("add");
+var btnUpdate = document.getElementById("update");
+var btnDelete = document.getElementById("delete");
+
+var roleSelect = document.getElementById("role_select");
+var txtWorkType = document.getElementById("work_type");
+
+var URL_REST_ROLE = "/timontey1/rest/role/";
+var URL_REST_WORK = "/timontey1/rest/work-type/";
+
+var ROLES = [];
+
+var $table = $('#table');
+var checkedRows = [];
