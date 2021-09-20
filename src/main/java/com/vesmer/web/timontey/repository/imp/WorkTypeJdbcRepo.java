a2e20a231d62d23c92e4dd5d870ad6a3cece8520
VasGorn <vas.goroshko@gmail.com>
2021-09-20 20:09:33 +0300

    feat: implement get list of work types by role from database

--- src/main/java/com/vesmer/web/timontey/repository/imp/WorkTypeJdbcRepo.java
+++ src/main/java/com/vesmer/web/timontey/repository/imp/WorkTypeJdbcRepo.java
@@ -8,10 +8,26 @@ import org.springframework.stereotype.Repository;
 
 import com.vesmer.web.timontey.domain.WorkType;
 import com.vesmer.web.timontey.repository.WorkTypeRepository;
+import com.vesmer.web.timontey.rowmapper.WorkTypeRowMapper;
 
 @Repository
 public class WorkTypeJdbcRepo implements WorkTypeRepository {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	private static final String SELECT_WORKTYPE_BY_ROLE_ID_SQL	= "SELECT w.id, w.work_name "
+																+ "FROM work_types AS w, "
+																+ "roles AS r, "
+																+ "role_worktype AS rw "
+																+ "WHERE w.id = rw.worktype_id"
+																+ "  AND r.id = rw.role_id"
+																+ "  AND r.id = ?;";
+
+	@Override
+	public List<WorkType> getWorkTypesByRole(long roleId) {
+		List<WorkType> list = (List<WorkType>) jdbcTemplate.query(SELECT_WORKTYPE_BY_ROLE_ID_SQL,
+				new WorkTypeRowMapper(), roleId);
+		return list;
+	}
 
 }
