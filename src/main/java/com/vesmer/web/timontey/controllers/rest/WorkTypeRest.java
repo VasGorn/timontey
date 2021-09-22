746e74f92574828732d6f6cbc53a4c45dd6d89f0
VasGorn <vas.goroshko@gmail.com>
2021-09-22 20:58:55 +0300

    feat: create post work type endpoint

--- src/main/java/com/vesmer/web/timontey/controllers/rest/WorkTypeRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/WorkTypeRest.java
@@ -3,9 +3,13 @@ package com.vesmer.web.timontey.controllers.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vesmer.web.timontey.domain.WorkType;
@@ -20,6 +24,12 @@ public class WorkTypeRest {
 	@GetMapping(path = "/role/{id}")
 	public List<WorkType> getWorkTypesByRole(@PathVariable("id") long roleId){
 		return workTypeService.getWorkTypesByRole(roleId);
+	}
+	
+	@PostMapping(path = "/role/{id}", consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public WorkType postWorkTypeToRole(@RequestBody WorkType workType, @PathVariable("id") long roleId) {
+		return workTypeService.save(workType, roleId);
 	}
 
 }
