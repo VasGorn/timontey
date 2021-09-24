c49685ded5dbb3ee9ec0730f695904c48029c87b
VasGorn <vas.goroshko@gmail.com>
2021-09-24 21:09:03 +0300

    feat: insert work type to database and return id

--- src/main/java/com/vesmer/web/timontey/repository/imp/WorkTypeJdbcRepo.java
+++ src/main/java/com/vesmer/web/timontey/repository/imp/WorkTypeJdbcRepo.java
@@ -1,9 +1,13 @@
 package com.vesmer.web.timontey.repository.imp;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.vesmer.web.timontey.domain.WorkType;
@@ -22,12 +26,34 @@ public class WorkTypeJdbcRepo implements WorkTypeRepository {
 																+ "WHERE w.id = rw.worktype_id"
 																+ "  AND r.id = rw.role_id"
 																+ "  AND r.id = ?;";
+	private static final String INSERT_SQL						= "INSERT INTO work_types (work_name) VALUES (?);";
 
 	@Override
 	public List<WorkType> getWorkTypesByRole(long roleId) {
 		List<WorkType> list = (List<WorkType>) jdbcTemplate.query(SELECT_WORKTYPE_BY_ROLE_ID_SQL,
 				new WorkTypeRowMapper(), roleId);
 		return list;
+	}
+	
+	private long insertWorkType(WorkType workType) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+
+	    jdbcTemplate.update(connection -> {
+	        PreparedStatement ps = connection
+	          .prepareStatement(INSERT_SQL, Statement.RETURN_GENERATED_KEYS);
+	          ps.setString(1, workType.getWorkTypeName());
+	          return ps;
+	        }, keyHolder);
+	    
+	    long newId;
+	    
+	    if (keyHolder.getKeys().size() > 1) {
+	        newId = ((Number) keyHolder.getKeys().get("id")).longValue();
+	    } else {
+	        newId= keyHolder.getKey().longValue();
+	    }
+	
+        return newId;
 	}
 
 }
