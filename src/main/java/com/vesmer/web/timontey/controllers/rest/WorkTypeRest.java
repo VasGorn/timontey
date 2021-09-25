272006cd38fd74caee3200d85837ac2b62d39136
VasGorn <vas.goroshko@gmail.com>
2021-09-25 21:12:34 +0300

    feat: add delete endpoint for work type

--- src/main/java/com/vesmer/web/timontey/controllers/rest/WorkTypeRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/WorkTypeRest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,12 @@ public class WorkTypeRest {
 	@ResponseStatus(HttpStatus.CREATED)
 	public WorkType postWorkTypeToRole(@RequestBody WorkType workType, @PathVariable("id") long roleId) {
 		return workTypeService.save(workType, roleId);
+	}
+	
+	@DeleteMapping("/{workTypeId}/role/{roleId}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteWorkType(@PathVariable("workTypeId") Long workTypeId, @PathVariable("roleId") Long roleId) {
+		workTypeService.delete(roleId, workTypeId);
 	}
 
 }
