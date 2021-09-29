cedf62fadb1239b40b064d27a600b7fd8e109ecf
VasGorn <vas.goroshko@gmail.com>
2021-09-29 20:48:30 +0300

    feat: add method signature to get work type by id in service interface

--- src/main/java/com/vesmer/web/timontey/service/WorkTypeService.java
+++ src/main/java/com/vesmer/web/timontey/service/WorkTypeService.java
@@ -1,6 +1,7 @@
 package com.vesmer.web.timontey.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.vesmer.web.timontey.domain.WorkType;
 
@@ -11,5 +12,7 @@ public interface WorkTypeService {
 	WorkType save(WorkType workType, long roleId);
 
 	void delete(Long roleId, Long workTypeId);
+
+	Optional<WorkType> getWorkTypeById(long id);
 
 }
