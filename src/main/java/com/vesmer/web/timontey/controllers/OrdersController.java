912da709b0591c5285641278fc9cf41142322a01
VasGorn <vas.goroshko@gmail.com>
2021-10-03 21:03:53 +0300

    feat: add orders controller

---
+++ src/main/java/com/vesmer/web/timontey/controllers/OrdersController.java
@@ -0,0 +1,28 @@
+package com.vesmer.web.timontey.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.vesmer.web.timontey.domain.Employee;
+import com.vesmer.web.timontey.service.StaffService;
+
+@Controller
+@RequestMapping("/orders")
+public class OrdersController {
+	@Autowired
+	private StaffService staffService;
+
+	@RequestMapping("/")
+	public ModelAndView getOrders() {
+		long managerId = 3;
+		Employee manager = staffService.getEmployeeById(managerId).get();
+		String fullName = manager.getLastName() + " " + manager.getFirstName() + " " + manager.getMiddleName();
+		
+		ModelAndView model = new ModelAndView("orders");
+		model.addObject("managerId", manager.getId());
+		model.addObject("managerName", fullName);
+		return model;
+	}
+}
