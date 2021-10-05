407f92af5c9c5cfd982dea6205e285863258627c
VasGorn <vas.goroshko@gmail.com>
2021-10-05 21:11:19 +0300

    feat: declare and initialize global variables in order javascript

--- src/main/webapp/static/js/orders.js
+++ src/main/webapp/static/js/orders.js
@@ -1,1 +1,13 @@
+var btnAdd = document.getElementById("add");
+var btnUpdate = document.getElementById("update");
+var btnDelete = document.getElementById("delete");
 
+var txtOrderName = document.getElementById("order_name");
+var hiddenManagerId = document.getElementById("manager_id");
+var txtDescription = document.getElementById("description");
+var txtAddress = document.getElementById("address");
+
+var URL_REST_ORDERS = "/timontey/rest/orders";
+
+var $table = $('#table');
+var checkedRows = [];
