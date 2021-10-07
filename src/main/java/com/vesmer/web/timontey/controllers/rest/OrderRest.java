8cbed27b5158a89a1fe770847a6ac97dcaa714de
VasGorn <vas.goroshko@gmail.com>
2021-10-07 21:20:10 +0300

    feat: create endpoint to get orders list by manager id

--- src/main/java/com/vesmer/web/timontey/controllers/rest/OrderRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/OrderRest.java
@@ -1,9 +1,14 @@
 package com.vesmer.web.timontey.controllers.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vesmer.web.timontey.domain.Order;
 import com.vesmer.web.timontey.service.OrderService;
 
 @RestController
@@ -11,5 +16,10 @@ import com.vesmer.web.timontey.service.OrderService;
 public class OrderRest {
 	@Autowired 
 	private OrderService orderService;
+	
+	@GetMapping("/{manager_id}")
+	public List<Order> getOrders(@PathVariable("manager_id") long managerId) {
+		return orderService.getOrders(managerId);
+	}
 
 }
