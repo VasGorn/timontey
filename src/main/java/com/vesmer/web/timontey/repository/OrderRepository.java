8075663a89170bb7179a4c46d6bb71078fe4507d
VasGorn <vas.goroshko@gmail.com>
2021-10-08 20:33:10 +0300

    feat: add get order method prototype in repository interface

---
+++ src/main/java/com/vesmer/web/timontey/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.vesmer.web.timontey.repository;
+
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.Employee;
+import com.vesmer.web.timontey.domain.Order;
+
+public interface OrderRepository {
+
+	List<Order> getOrders(Employee manager);
+
+}
