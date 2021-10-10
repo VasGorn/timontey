6d9f917ea1415a1216006ed43a63f394a0406578
VasGorn <vas.goroshko@gmail.com>
2021-10-10 21:05:29 +0300

    feat: create endpoint to save order

--- src/main/java/com/vesmer/web/timontey/controllers/rest/OrderRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/OrderRest.java
@@ -3,9 +3,13 @@ package com.vesmer.web.timontey.controllers.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vesmer.web.timontey.domain.Order;
@@ -20,6 +24,12 @@ public class OrderRest {
 	@GetMapping("/{manager_id}")
 	public List<Order> getOrders(@PathVariable("manager_id") long managerId) {
 		return orderService.getOrders(managerId);
+	}
+	
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Order postOrder(@RequestBody Order order) {
+		return orderService.save(order);
 	}
 
 }
