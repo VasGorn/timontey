24c79e15995254ce18e375ff02b7320b50ec613c
VasGorn <vas.goroshko@gmail.com>
2021-10-21 21:36:24 +0300

    feat: create checkboxes according roles

--- src/main/webapp/static/js/users.js
+++ src/main/webapp/static/js/users.js
@@ -20,3 +20,38 @@ var URL_REST_USERS = "/timontey/rest/users/";
 btnAdd.disabled = true;
 btnUpdate.disabled = true;
 btnDelete.disabled = true;
+
+setDataToSelectRole();
+
+function setDataToSelectRole() {
+	$.ajax({
+		type: "GET",
+		url: URL_REST_ROLE + "all",
+		data: null,
+		success: function(resp) {
+			console.log(resp);
+			for (let i = 0; i < resp.length; ++i) {
+				const newDiv = document.createElement("div");
+				newDiv.classList.add("form-check");
+				newDiv.classList.add("form-check-inline");
+
+				const checkbox = document.createElement("input");
+				checkbox.type = "checkbox";
+				checkbox.name = "roles";
+				checkbox.value = resp[i].id;
+				checkbox.classList.add("form-check-input");
+
+				const label = document.createElement("label");
+				label.htmlFor = "roles";
+				label.classList.add("form-check-label");
+
+				label.appendChild(document.createTextNode(resp[i].roleName));
+				newDiv.appendChild(checkbox);
+				newDiv.appendChild(label);
+
+				checkBoxContainer.appendChild(newDiv);
+			}
+			checkBoxRoles = document.getElementsByName("roles");
+		}
+	});
+}
