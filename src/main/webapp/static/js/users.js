9f982647738b168ec420e3ea905a7e23020c96d0
VasGorn <vas.goroshko@gmail.com>
2021-10-21 21:01:31 +0300

    feat: define global variables in user javascript

--- src/main/webapp/static/js/users.js
+++ src/main/webapp/static/js/users.js
@@ -1,1 +1,18 @@
-console.log("TODO users javascript!");
+var txtUsername = document.getElementById("username");
+var txtPassword = document.getElementById("password");
+var txtRepeatPassword = document.getElementById("confirm_password");
+
+var checkBoxContainer = document.getElementById("check_role");
+var selectEmployee = document.getElementById("select_employee");
+var checkBoxRoles;
+
+var btnAdd = document.getElementById("add");
+var btnUpdate = document.getElementById("update");
+var btnDelete = document.getElementById("delete");
+
+var $table = $('#table');
+var checkedRows = [];
+
+var URL_REST_ROLE = "/timontey/rest/role/";
+var URL_REST_STAFF = "/timontey/rest/staff/";
+var URL_REST_USERS = "/timontey/rest/users/";
