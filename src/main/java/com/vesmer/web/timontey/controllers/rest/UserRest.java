b52d15e334216cce6b08e19bad673e0e11c8653f
VasGorn <vas.goroshko@gmail.com>
2021-10-23 20:17:05 +0300

    feat: create rest endpoint to get all users

--- src/main/java/com/vesmer/web/timontey/controllers/rest/UserRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/UserRest.java
@@ -1,9 +1,13 @@
 package com.vesmer.web.timontey.controllers.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vesmer.web.timontey.domain.User;
 import com.vesmer.web.timontey.service.UserService;
 
 @RestController
@@ -11,6 +15,10 @@ import com.vesmer.web.timontey.service.UserService;
 public class UserRest {
 	@Autowired
 	private UserService userService;
-
+	
+	@GetMapping("/all")
+	public List<User> getAllUsers() {
+		return userService.getAll();
+	}
 
 }
