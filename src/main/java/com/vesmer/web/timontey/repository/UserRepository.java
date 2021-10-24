9cc62cf038d27839cc9ef212145ed562c4e3b3c7
VasGorn <vas.goroshko@gmail.com>
2021-10-24 20:18:52 +0300

    feat: create user repository interface and add get all method signature

---
+++ src/main/java/com/vesmer/web/timontey/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.vesmer.web.timontey.repository;
+
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.User;
+
+public interface UserRepository {
+
+	List<User> getAll();
+
+}
