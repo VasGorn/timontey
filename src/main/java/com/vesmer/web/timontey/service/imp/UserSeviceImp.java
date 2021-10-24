9bb74fd1552c36c2739fc2a951a50e8ce7e3b331
VasGorn <vas.goroshko@gmail.com>
2021-10-24 20:17:24 +0300

    feat: create user service class and implement get all user list method

---
+++ src/main/java/com/vesmer/web/timontey/service/imp/UserSeviceImp.java
@@ -0,0 +1,54 @@
+package com.vesmer.web.timontey.service.imp;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vesmer.web.timontey.domain.Employee;
+import com.vesmer.web.timontey.domain.Role;
+import com.vesmer.web.timontey.domain.User;
+import com.vesmer.web.timontey.repository.RoleRepository;
+import com.vesmer.web.timontey.repository.StaffRepository;
+import com.vesmer.web.timontey.repository.UserRepository;
+import com.vesmer.web.timontey.service.UserService;
+
+@Service
+@Transactional
+public class UserSeviceImp implements UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private StaffRepository staffRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+
+
+	@Override
+	public List<User> getAll() {
+		List<User> userList = userRepository.getAll();
+		for (int i = 0; i < userList.size(); ++i) {
+			User user = userList.get(i);
+
+			long employeeId = user.getId();
+			Optional<Employee> optEmployee = staffRepository.findById(employeeId);
+			if (optEmployee.isPresent()) {
+				Employee employee = optEmployee.get();
+				user.setEmployee(employee);
+			} else {
+				user.setFirstName("not found");
+			}
+
+			List<Role> roles = roleRepository.getRolesForUser(user);
+			user.setRoles(roles);
+		}
+
+		return userList;
+	}
+
+}
