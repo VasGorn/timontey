d0633b720d59c38ac0078292ae96aec739a79689
VasGorn <vas.goroshko@gmail.com>
2021-10-25 20:29:39 +0300

    feat: implement method to get list of roles for user

--- src/main/java/com/vesmer/web/timontey/repository/imp/RoleJdbcRepo.java
+++ src/main/java/com/vesmer/web/timontey/repository/imp/RoleJdbcRepo.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.vesmer.web.timontey.domain.Role;
+import com.vesmer.web.timontey.domain.User;
 import com.vesmer.web.timontey.repository.RoleRepository;
 import com.vesmer.web.timontey.rowmapper.RoleRowMapper;
 
@@ -16,12 +17,24 @@ public class RoleJdbcRepo implements RoleRepository {
 	private JdbcTemplate jdbcTemplate;
 
 	private static final String SELECT_ALL_SQL	= "SELECT * FROM roles;";
+	private static final String SELECT_ROLES_FOR_USER_SQL =
+		"SELECT r.id, r.role_name "
+		+ "FROM roles AS r, user_role AS ur "
+		+ "WHERE r.id = ur.role_id "
+		+ "AND ur.username = ?;";
 
 	@Override
 	public List<Role> getAll() {
 		List<Role> roleList = (List<Role>) jdbcTemplate.query(SELECT_ALL_SQL,
 				new RoleRowMapper());
 		return roleList;
+	}
+	
+	@Override
+	public List<Role> getRolesForUser(User user) {
+		List<Role> list = (List<Role>) jdbcTemplate.query(SELECT_ROLES_FOR_USER_SQL,
+				new RoleRowMapper(), user.getUsername());
+		return list;
 	}
 
 }
