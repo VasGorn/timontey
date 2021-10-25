395f4d19af9a6698b8cd04cb160f9b8b23b62a34
VasGorn <vas.goroshko@gmail.com>
2021-10-25 20:05:55 +0300

    feat: implement get all users from datebase

--- src/main/java/com/vesmer/web/timontey/repository/imp/UserJdbcRepo.java
+++ src/main/java/com/vesmer/web/timontey/repository/imp/UserJdbcRepo.java
@@ -8,10 +8,21 @@ import org.springframework.stereotype.Repository;
 
 import com.vesmer.web.timontey.domain.User;
 import com.vesmer.web.timontey.repository.UserRepository;
+import com.vesmer.web.timontey.rowmapper.UserRowMapper;
 
 @Repository
 public class UserJdbcRepo implements UserRepository {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	private static final String SELECT_ALL_SQL =
+		"SELECT * FROM users;";
+
+	@Override
+	public List<User> getAll() {
+		List<User> list = (List<User>) jdbcTemplate.query(SELECT_ALL_SQL,
+				new UserRowMapper());
+		return list;
+	}
 
 }
