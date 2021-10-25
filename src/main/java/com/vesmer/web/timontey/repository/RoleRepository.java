9cb9ad34a85275702ffefb1dd5cea8384c908b1c
VasGorn <vas.goroshko@gmail.com>
2021-10-25 20:08:37 +0300

    feat: add method signature to role repository interface to get roles for user

--- src/main/java/com/vesmer/web/timontey/repository/RoleRepository.java
+++ src/main/java/com/vesmer/web/timontey/repository/RoleRepository.java
@@ -3,9 +3,12 @@ package com.vesmer.web.timontey.repository;
 import java.util.List;
 
 import com.vesmer.web.timontey.domain.Role;
+import com.vesmer.web.timontey.domain.User;
 
 public interface RoleRepository {
 
 	List<Role> getAll();
+
+	List<Role> getRolesForUser(User user);
 
 }
