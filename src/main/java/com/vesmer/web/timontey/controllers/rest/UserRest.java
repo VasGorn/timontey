2a133e842d0e896f4c748d1c01754750ce8927ce
VasGorn <vas.goroshko@gmail.com>
2021-10-28 20:27:00 +0300

    feat: create endpoint to save user

--- src/main/java/com/vesmer/web/timontey/controllers/rest/UserRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/UserRest.java
@@ -3,8 +3,12 @@ package com.vesmer.web.timontey.controllers.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vesmer.web.timontey.domain.User;
@@ -19,6 +23,12 @@ public class UserRest {
 	@GetMapping("/all")
 	public List<User> getAllUsers() {
 		return userService.getAll();
+	}
+	
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public User postUser(@RequestBody User user) {
+		return userService.save(user);
 	}
 
 }
