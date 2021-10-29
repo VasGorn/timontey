d3f30d49c7e1ec1f58410c86fae4275b741e87f0
VasGorn <vas.goroshko@gmail.com>
2021-10-29 21:13:19 +0300

    feat: add method signature in user service interface to find user by username

--- src/main/java/com/vesmer/web/timontey/service/UserService.java
+++ src/main/java/com/vesmer/web/timontey/service/UserService.java
@@ -1,6 +1,7 @@
 package com.vesmer.web.timontey.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.vesmer.web.timontey.domain.User;
 
@@ -9,5 +10,7 @@ public interface UserService {
 	List<User> getAll();
 
 	User save(User user);
+	
+	Optional<User> getUserByUsername(String username);
 
 }
