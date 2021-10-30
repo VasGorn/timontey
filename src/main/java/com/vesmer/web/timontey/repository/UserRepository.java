e25d0d29cc7ffb6341c9311adff8b1b2156d3ddd
VasGorn <vas.goroshko@gmail.com>
2021-10-30 20:08:18 +0300

    feat: add get user by username method signature in user repository interface

--- src/main/java/com/vesmer/web/timontey/repository/UserRepository.java
+++ src/main/java/com/vesmer/web/timontey/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.vesmer.web.timontey.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.vesmer.web.timontey.domain.User;
 
@@ -9,5 +10,7 @@ public interface UserRepository {
 	List<User> getAll();
 
 	User save(User user);
+
+	Optional<User> getUserByUsername(String username);
 
 }
