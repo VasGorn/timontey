1dccd1945eaf0a2ddda959190e3b970d7fcdafcd
VasGorn <vas.goroshko@gmail.com>
2021-11-12 18:31:56 +0200

    feat: create endpoint to get quota of money by manager id

--- src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaMoneyRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaMoneyRest.java
@@ -1,9 +1,14 @@
 package com.vesmer.web.timontey.controllers.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vesmer.web.timontey.domain.QuotaMoney;
 import com.vesmer.web.timontey.service.QuotaMoneyService;
 
 @RestController
@@ -11,5 +16,11 @@ import com.vesmer.web.timontey.service.QuotaMoneyService;
 public class QuotaMoneyRest {
 	@Autowired
 	private QuotaMoneyService quotaMoneyService;
+	
+	@GetMapping("/manager/{managerId}")
+	public List<QuotaMoney> getQuotaMoneyForManager(
+			@PathVariable("managerId") long managerId){
+		return quotaMoneyService.getQuotaMoneysForManager(managerId);
+	}
 
 }
