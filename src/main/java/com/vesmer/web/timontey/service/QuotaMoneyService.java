ec0cc39bf63fdc5b151210ec9c56159daea237d8
VasGorn <vas.goroshko@gmail.com>
2021-11-15 20:51:43 +0200

    feat: add method signature to get quota of money by manager id in service interface

--- src/main/java/com/vesmer/web/timontey/service/QuotaMoneyService.java
+++ src/main/java/com/vesmer/web/timontey/service/QuotaMoneyService.java
@@ -1,5 +1,11 @@
 package com.vesmer.web.timontey.service;
 
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.QuotaMoney;
+
 public interface QuotaMoneyService {
+
+	List<QuotaMoney> getQuotaMoneysForManager(long managerId);
 
 }
