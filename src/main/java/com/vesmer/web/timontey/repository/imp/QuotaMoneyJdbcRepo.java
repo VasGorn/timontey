6f602fb8495c5b4d248a4e4fddaf541dd1f7d864
VasGorn <vas.goroshko@gmail.com>
2021-11-15 21:17:50 +0200

    feat: create jdbc class implementation for quota of money

---
+++ src/main/java/com/vesmer/web/timontey/repository/imp/QuotaMoneyJdbcRepo.java
@@ -0,0 +1,10 @@
+package com.vesmer.web.timontey.repository.imp;
+
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.QuotaMoney;
+import com.vesmer.web.timontey.repository.QuotaMoneyRepository;
+
+public class QuotaMoneyJdbcRepo implements QuotaMoneyRepository {
+
+}
