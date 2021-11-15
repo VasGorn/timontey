3ce599da0e1e3d611129972526c57eff9cc1bfed
VasGorn <vas.goroshko@gmail.com>
2021-11-15 21:04:40 +0200

    feat: implement method to get list of quota money by manager id in service layer

--- src/main/java/com/vesmer/web/timontey/service/imp/QuotaMoneyServiceImp.java
+++ src/main/java/com/vesmer/web/timontey/service/imp/QuotaMoneyServiceImp.java
@@ -1,7 +1,24 @@
 package com.vesmer.web.timontey.service.imp;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vesmer.web.timontey.domain.QuotaMoney;
+import com.vesmer.web.timontey.repository.QuotaMoneyRepository;
 import com.vesmer.web.timontey.service.QuotaMoneyService;
 
 public class QuotaMoneyServiceImp implements QuotaMoneyService{
+	@Autowired
+	private QuotaMoneyRepository quotaMoneyRepository;
+
+	@Override
+	public List<QuotaMoney> getQuotaMoneysForManager(long managerId) {
+		List<QuotaMoney> quotaMoneyList = quotaMoneyRepository.getQuotaMoneysForManager(managerId);
+		for(QuotaMoney qMoney: quotaMoneyList) {
+			patch(qMoney);
+		}
+		return quotaMoneyList;
+	}
 
 }
