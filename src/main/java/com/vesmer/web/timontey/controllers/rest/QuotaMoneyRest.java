e0167e719b17080a58669d78ed9dc770912e9c13
VasGorn <vas.goroshko@gmail.com>
2021-11-19 21:18:14 +0200

    feat: create endpoint to save quota of money

--- src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaMoneyRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaMoneyRest.java
@@ -3,9 +3,13 @@ package com.vesmer.web.timontey.controllers.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vesmer.web.timontey.domain.QuotaMoney;
@@ -21,6 +25,12 @@ public class QuotaMoneyRest {
 	public List<QuotaMoney> getQuotaMoneyForManager(
 			@PathVariable("managerId") long managerId){
 		return quotaMoneyService.getQuotaMoneysForManager(managerId);
+	}
+	
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public QuotaMoney postQuotaMoney(@RequestBody QuotaMoney quotaMoney) {
+		return quotaMoneyService.save(quotaMoney);
 	}
 
 }
