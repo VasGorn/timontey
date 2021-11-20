2a0d95f7fbfd1db1f7384f7f3107205ef6c3c423
VasGorn <vas.goroshko@gmail.com>
2021-11-20 20:55:41 +0200

    feat: implement save quota money method in jdbc repository class

--- src/main/java/com/vesmer/web/timontey/repository/imp/QuotaMoneyJdbcRepo.java
+++ src/main/java/com/vesmer/web/timontey/repository/imp/QuotaMoneyJdbcRepo.java
@@ -1,9 +1,13 @@
 package com.vesmer.web.timontey.repository.imp;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.vesmer.web.timontey.domain.QuotaMoney;
@@ -21,11 +25,37 @@ public class QuotaMoneyJdbcRepo implements QuotaMoneyRepository {
 		+ "orders AS o "
 		+ "WHERE o.id=q.order_id AND "
 		+ "o.manager_id=?;";
+	private static final String INSERT_QUOTA_MONEY_SQL	= 
+			"INSERT INTO quota_money (order_id, employee_id, "
+			+ "money_limit) VALUES (?, ?, ?);";
 
 	@Override
 	public List<QuotaMoney> getQuotaMoneysForManager(long managerId) {
 		return jdbcTemplate.query(SELECT_QUOTA_MONEY_BY_MANAGER_ID_SQL, 
 				new QuotaMoneyRowMapper(), managerId);
+	}
+
+	@Override
+	public long save(QuotaMoney quotaMoney) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		
+	    jdbcTemplate.update(connection -> {
+	        PreparedStatement ps = connection
+	          .prepareStatement(INSERT_QUOTA_MONEY_SQL, Statement.RETURN_GENERATED_KEYS);
+	          ps.setLong(1, quotaMoney.getOrder().getId());
+	          ps.setLong(2, quotaMoney.getEmployee().getId());
+	          ps.setFloat(3, quotaMoney.getMoneyLimit());
+	          return ps;
+	        }, keyHolder);
+	    
+	    long newId;
+	    if (keyHolder.getKeys().size() > 1) {
+	        newId = ((Number) keyHolder.getKeys().get("id")).longValue();
+	    } else {
+	        newId= keyHolder.getKey().longValue();
+	    }
+
+        return newId;
 	}
 
 }
