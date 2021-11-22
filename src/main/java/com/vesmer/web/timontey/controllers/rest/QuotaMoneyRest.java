9ad80cbc6d160a5c1c1f8662195171980e82b202
VasGorn <vas.goroshko@gmail.com>
2021-11-22 19:03:57 +0200

    feat: create endpoint for put request to update quota of money

--- src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaMoneyRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaMoneyRest.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,6 +32,11 @@ public class QuotaMoneyRest {
 	@ResponseStatus(HttpStatus.CREATED)
 	public QuotaMoney postQuotaMoney(@RequestBody QuotaMoney quotaMoney) {
 		return quotaMoneyService.save(quotaMoney);
+	}
+	
+	@PutMapping("/{quotaMoneyId}")
+	public QuotaMoney putQuotaMoney(@RequestBody QuotaMoney quotaMoney) {
+		return quotaMoneyService.update(quotaMoney);
 	}
 
 }
