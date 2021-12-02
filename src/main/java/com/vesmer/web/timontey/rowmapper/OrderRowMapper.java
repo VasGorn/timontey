c09fc79a9417bcf5b9b1eb754342f28669dafc9c
VasGorn <vas.goroshko@gmail.com>
2021-12-02 20:18:19 +0200

    feat: set manager to order in the order row mapper

--- src/main/java/com/vesmer/web/timontey/rowmapper/OrderRowMapper.java
+++ src/main/java/com/vesmer/web/timontey/rowmapper/OrderRowMapper.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
 
+import com.vesmer.web.timontey.domain.Employee;
 import com.vesmer.web.timontey.domain.Order;
 
 
@@ -15,6 +16,11 @@ public class OrderRowMapper implements RowMapper<Order> {
 		Order order = new Order();
 		order.setId(rs.getLong("id"));
 		order.setName(rs.getString("name"));
+		
+		Employee manager = new Employee();
+		manager.setId(rs.getLong("manager_id"));
+		order.setManager(manager);
+		
 		order.setDescription(rs.getString("description"));
 		order.setAddress(rs.getString("address"));
 		
