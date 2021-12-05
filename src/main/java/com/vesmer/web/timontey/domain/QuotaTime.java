3ac1939cd1ffad3830856acc7199ac65d25aa270
VasGorn <vas.goroshko@gmail.com>
2021-12-05 20:12:33 +0200

    feat: create class for quota of time on order, employee, year and list of work types with hours

---
+++ src/main/java/com/vesmer/web/timontey/domain/QuotaTime.java
@@ -0,0 +1,46 @@
+package com.vesmer.web.timontey.domain;
+
+import java.util.List;
+
+public class QuotaTime {
+	private long id;
+	private Order order;
+	private Employee employee;
+	private short year;
+	private List<WorkTypeHours> workTypeHours;
+	
+	public QuotaTime() {
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	public short getYear() {
+		return year;
+	}
+	public void setYear(short year) {
+		this.year = year;
+	}
+	public List<WorkTypeHours> getWorkTypeHours() {
+		return workTypeHours;
+	}
+	public void setWorkTypeHours(List<WorkTypeHours> workTypeHours) {
+		this.workTypeHours = workTypeHours;
+	}
+
+}
