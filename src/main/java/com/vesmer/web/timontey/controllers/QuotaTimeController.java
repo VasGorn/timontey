d817dd6d9c00a655a38122bc345cb90cb683bb10
VasGorn <vas.goroshko@gmail.com>
2021-12-06 20:46:55 +0200

    feat: create controller class for quota of time

---
+++ src/main/java/com/vesmer/web/timontey/controllers/QuotaTimeController.java
@@ -0,0 +1,14 @@
+package com.vesmer.web.timontey.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.vesmer.web.timontey.service.StaffService;
+
+@Controller
+public class QuotaTimeController {
+	@Autowired
+	private StaffService staffService;
+	
+
+}
