ced9e9d2018b4352db5b46973a2799f409829f6c
VasGorn <vas.goroshko@gmail.com>
2021-12-10 19:44:42 +0200

    feat: define global variables in quota of time javascript

---
+++ src/main/webapp/static/js/quota-time.js
@@ -0,0 +1,30 @@
+var btnAdd = document.getElementById("add");
+var btnUpdate = document.getElementById("update");
+var btnDelete = document.getElementById("delete");
+
+var selectOrder = document.getElementById("select_order");
+var txtOrderName = document.getElementById("order_name");
+var hiddenManagerId = document.getElementById("manager_id");
+var txtDescription = document.getElementById("description");
+var txtAddress = document.getElementById("address");
+
+var selectRole = document.getElementById("select_role");
+var selectEmployee = document.getElementById("select_employee");
+var selectWorkType = document.getElementById("select_work_type");
+var selectYear = document.getElementById("select_year");
+var selectMonth = document.getElementById("select_month");
+var numHours = document.getElementById("hours_limit");
+
+var numMoneyLimit = document.getElementById("money_limit");
+
+var URL_REST_ROLE = "/timontey1/rest/role";
+var URL_REST_EMPLOYEE = "/timontey1/rest/staff";
+var URL_REST_WORK = "/timontey1/rest/work-type";
+var URL_REST_ORDER = "/timontey1/rest/orders";
+var URL_REST_QUOTA_TIME = "/timontey1/rest/quota-time";
+
+var ROLES = [];
+var ORDERS = [];
+
+var $table = $('#table');
+var checkedRows = [];
