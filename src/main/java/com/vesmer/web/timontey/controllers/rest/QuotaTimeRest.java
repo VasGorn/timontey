daff6cd843fd764c7b899f55cf3e238e136bee1a
VasGorn <vas.goroshko@gmail.com>
2021-12-16 20:54:32 +0200

    feat: add endpoint to get quota of time list for order

--- src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaTimeRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaTimeRest.java
@@ -1,9 +1,14 @@
 package com.vesmer.web.timontey.controllers.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vesmer.web.timontey.domain.QuotaTime;
 import com.vesmer.web.timontey.service.QuotaTimeService;
 
 @RestController
@@ -11,5 +16,13 @@ import com.vesmer.web.timontey.service.QuotaTimeService;
 public class QuotaTimeRest {
 	@Autowired
 	private QuotaTimeService quotaTimeService;
+	
+	@GetMapping("/order/{orderId}/month/{numMonth}/year/{year}")
+	public List<QuotaTime> getQuotaTimeListForOrder(
+			@PathVariable("orderId") long orderId,
+			@PathVariable("numMonth") short numMonth, 
+			@PathVariable("year") short year){
+		return quotaTimeService.getQuotaTimeListForOrder(orderId, numMonth, year);
+	}
 
 }
