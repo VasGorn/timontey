e82a2f48002f72cda1b160f285f12443805b1988
VasGorn <vas.goroshko@gmail.com>
2021-12-19 19:29:48 +0200

    feat: implement method to get list of quota time for order in repository class with jdbc realization

--- src/main/java/com/vesmer/web/timontey/repository/imp/QuotaTimeJdbcRepo.java
+++ src/main/java/com/vesmer/web/timontey/repository/imp/QuotaTimeJdbcRepo.java
@@ -1,5 +1,27 @@
 package com.vesmer.web.timontey.repository.imp;
 
-public class QuotaTimeJdbcRepo {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.vesmer.web.timontey.domain.QuotaTime;
+import com.vesmer.web.timontey.repository.QuotaTimeRepository;
+
+@Repository
+public class QuotaTimeJdbcRepo implements QuotaTimeRepository{
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	private static final String SELECT_QUOTA_TIME_LIST_FOR_ORDER_SQL =
+		"SELECT * FROM quota_time WHERE order_id=? AND year=?;";
+
+	@Override
+	public List<QuotaTime> getQuotaTimeListForOrder(long orderId, 
+			short numMonth, short year) {
+		return getQuotaTimeList(SELECT_QUOTA_TIME_LIST_FOR_ORDER_SQL,
+				orderId, numMonth, year);
+	}
 
 }
