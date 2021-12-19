eb29318011f4eab05930919fafa7e2eb9d5ed099
VasGorn <vas.goroshko@gmail.com>
2021-12-19 19:20:48 +0200

    feat: add method signature to get quota of time list for order in repository interface

--- src/main/java/com/vesmer/web/timontey/repository/QuotaTimeRepository.java
+++ src/main/java/com/vesmer/web/timontey/repository/QuotaTimeRepository.java
@@ -1,5 +1,12 @@
 package com.vesmer.web.timontey.repository;
 
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.QuotaTime;
+
 public interface QuotaTimeRepository {
+
+	List<QuotaTime> getQuotaTimeListForOrder(long orderId, short numMonth, 
+			short year);
 
 }
