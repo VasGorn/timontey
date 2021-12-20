e69506719dfba39a0c51e6349f459706b73109d6
VasGorn <vas.goroshko@gmail.com>
2021-12-20 20:17:56 +0200

    feat: get quota time list by sql, paramenter id, number of month and year

--- src/main/java/com/vesmer/web/timontey/repository/imp/QuotaTimeJdbcRepo.java
+++ src/main/java/com/vesmer/web/timontey/repository/imp/QuotaTimeJdbcRepo.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import com.vesmer.web.timontey.domain.QuotaTime;
 import com.vesmer.web.timontey.repository.QuotaTimeRepository;
+import com.vesmer.web.timontey.rowmapper.QuotaTimeRowMapper;
 
 @Repository
 public class QuotaTimeJdbcRepo implements QuotaTimeRepository{
@@ -22,6 +23,19 @@ public class QuotaTimeJdbcRepo implements QuotaTimeRepository{
 			short numMonth, short year) {
 		return getQuotaTimeList(SELECT_QUOTA_TIME_LIST_FOR_ORDER_SQL,
 				orderId, numMonth, year);
+	}
+	
+	private List<QuotaTime> getQuotaTimeList(String sql, long paramerId, 
+			short numMonth, short year){
+		List<QuotaTime> listQuota = jdbcTemplate.query(sql, 
+				new QuotaTimeRowMapper(), paramerId, year);
+		
+		for(int i = 0; i < listQuota.size(); ++i) {
+			long quotaId =  listQuota.get(i).getId();
+			listQuota.get(i).setWorkTypeHours(getWorktypeQuotaList(quotaId, numMonth));
+		}
+		
+		return listQuota;
 	}
 
 }
