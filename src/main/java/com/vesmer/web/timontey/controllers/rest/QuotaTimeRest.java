f418c4bd7502628cd7a67d16172cccfa0cf14ca0
VasGorn <vas.goroshko@gmail.com>
2021-12-25 19:54:08 +0200

    feat: add endpoint to post with request body the quota of time and save record

--- src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaTimeRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaTimeRest.java
@@ -3,9 +3,13 @@ package com.vesmer.web.timontey.controllers.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vesmer.web.timontey.domain.QuotaTime;
@@ -23,6 +27,12 @@ public class QuotaTimeRest {
 			@PathVariable("numMonth") short numMonth, 
 			@PathVariable("year") short year){
 		return quotaTimeService.getQuotaTimeListForOrder(orderId, numMonth, year);
+	}
+		
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public QuotaTime postQuotaTime(@RequestBody QuotaTime quotaTime) {
+		return quotaTimeService.save(quotaTime);
 	}
 
 }
