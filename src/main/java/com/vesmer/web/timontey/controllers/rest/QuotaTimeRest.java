814a144fab7c84b25160abd77d5b3e4a72483dd9
VasGorn <vas.goroshko@gmail.com>
2022-01-03 19:47:01 +0200

    feat: create endpoint for put request to update quota of time

--- src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaTimeRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaTimeRest.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -34,5 +35,12 @@ public class QuotaTimeRest {
 	public QuotaTime postQuotaTime(@RequestBody QuotaTime quotaTime) {
 		return quotaTimeService.save(quotaTime);
 	}
+	
+	@PutMapping("/{quotaTimeId}")
+	public QuotaTime putQuotaTime(@PathVariable("quotaTimeId") long quotaTimeId,
+			@RequestBody QuotaTime quotaTime) {
+		return quotaTimeService.update(quotaTime);
+	}
+
 
 }
