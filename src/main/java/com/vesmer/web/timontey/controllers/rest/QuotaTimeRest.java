46257a943f698c3faff69d8d047557fb4244b42d
VasGorn <vas.goroshko@gmail.com>
2022-01-05 22:03:54 +0200

    feat: create endpoint to delete work hours quota by id number

--- src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaTimeRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/QuotaTimeRest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,11 @@ public class QuotaTimeRest {
 			@RequestBody QuotaTime quotaTime) {
 		return quotaTimeService.update(quotaTime);
 	}
-
+	
+	@DeleteMapping("/workHours/{workHoursId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteWorkHours(@PathVariable("workHoursId") long workHoursId) {
+		quotaTimeService.delete(workHoursId);
+	}
 
 }
