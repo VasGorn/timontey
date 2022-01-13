86e4ba5548417805d9d2d1504741d01285da2cc5
VasGorn <vas.goroshko@gmail.com>
2022-01-13 19:56:15 +0200

    feat: add endpoint to get view of spend money

--- src/main/java/com/vesmer/web/timontey/controllers/SpendMoneyController.java
+++ src/main/java/com/vesmer/web/timontey/controllers/SpendMoneyController.java
@@ -1,8 +1,20 @@
 package com.vesmer.web.timontey.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SpendMoneyController {
+	
+	@RequestMapping("/spend-money")
+	public ModelAndView getSpendMoneyPage() {
+		long employeeId = 12;
+		
+		ModelAndView model = new ModelAndView("spend-money");
+		model.addObject("employeeId", employeeId);
+		
+		return model;
+	}
 
 }
