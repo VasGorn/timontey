f6aeeb3333b4c0c0fe56bf25be07aa5dc1b67bd3
VasGorn <vas.goroshko@gmail.com>
2022-01-14 20:33:04 +0200

    feat: declare global variables in javascript file for spending money view

---
+++ src/main/webapp/static/js/spend-money.js
@@ -0,0 +1,27 @@
+var btnAdd = document.getElementById("add");
+var btnUpdate = document.getElementById("update");
+var btnDelete = document.getElementById("delete");
+
+var hEmployeeId = document.getElementById("employee_id");
+
+var selectOrder = document.getElementById("select_order");
+var txtManagerName = document.getElementById("manager_name");
+var txtDescription = document.getElementById("description");
+var txtAddress = document.getElementById("address");
+
+var numOrderBalanceMoney = document.getElementById("order_balance_money");
+var numOrderLimitMoney = document.getElementById("order_limit_money");
+
+var selectExpenses = document.getElementById("select_expenses");
+var numMoneySpend = document.getElementById("money_spend");
+
+var $table = $('#table');
+
+var URL_REST_SPEND_MONEY = "/timontey/rest/spend-money";
+var URL_REST_EXPENSES = "/timontey/rest/expenses";
+
+var QUOTA_MONEY_ARRAY = [];
+var LIMIT_MONEY = 0.0;
+var BALANCE_MONEY = 0.0;
+
+var checkedRows = [];
