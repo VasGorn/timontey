4ea942f348b7fffc587d9a73635860f00760fe62
VasGorn <vas.goroshko@gmail.com>
2022-01-14 21:00:45 +0200

    feat: create endpoint to get money spend by employee id number

--- src/main/java/com/vesmer/web/timontey/controllers/rest/SpendMoneyRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/SpendMoneyRest.java
@@ -1,9 +1,14 @@
 package com.vesmer.web.timontey.controllers.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vesmer.web.timontey.domain.MoneySpend;
 import com.vesmer.web.timontey.service.SpendMoneyService;
 
 @RestController
@@ -11,5 +16,11 @@ import com.vesmer.web.timontey.service.SpendMoneyService;
 public class SpendMoneyRest {
 	@Autowired
 	private SpendMoneyService spendMoneyService;
+	
+	@GetMapping("/employee/{employeeId}")
+	public List<MoneySpend> getMoneySpendList(
+			@PathVariable("employeeId") long employeeId){
+		return spendMoneyService.getMoneySpendListForEmployee(employeeId);
+	}
 
 }
