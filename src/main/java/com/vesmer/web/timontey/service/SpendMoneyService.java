b04cf4dfa8a78b21581439bf22f6edb2f8aec735
VasGorn <vas.goroshko@gmail.com>
2022-01-15 20:15:31 +0200

    feat: add method signature to get money spend list by employee id

--- src/main/java/com/vesmer/web/timontey/service/SpendMoneyService.java
+++ src/main/java/com/vesmer/web/timontey/service/SpendMoneyService.java
@@ -1,5 +1,11 @@
 package com.vesmer.web.timontey.service;
 
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.MoneySpend;
+
 public interface SpendMoneyService {
+
+	List<MoneySpend> getMoneySpendListForEmployee(long employeeId);
 
 }
