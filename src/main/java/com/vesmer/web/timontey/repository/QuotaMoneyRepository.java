b2b0f71e6e8941e42cd9097d64335b438f77101c
VasGorn <vas.goroshko@gmail.com>
2022-01-17 19:33:52 +0200

    feat: add method signature to find quota of money by id in repository interface

--- src/main/java/com/vesmer/web/timontey/repository/QuotaMoneyRepository.java
+++ src/main/java/com/vesmer/web/timontey/repository/QuotaMoneyRepository.java
@@ -1,6 +1,7 @@
 package com.vesmer.web.timontey.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.vesmer.web.timontey.domain.QuotaMoney;
 
@@ -13,5 +14,7 @@ public interface QuotaMoneyRepository {
 	int update(QuotaMoney quotaMoney);
 
 	void delete(long quotaMoneyId);
+
+	Optional<QuotaMoney> findQuotaMoneyById(long id);
 
 }
