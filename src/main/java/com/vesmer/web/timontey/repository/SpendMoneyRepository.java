9471c06620198a070bdcb17abf2d52f1bb4f1f0d
VasGorn <vas.goroshko@gmail.com>
2022-01-19 20:57:05 +0200

    feat: add method signature to get money expense list by quota id in repository interface

--- src/main/java/com/vesmer/web/timontey/repository/SpendMoneyRepository.java
+++ src/main/java/com/vesmer/web/timontey/repository/SpendMoneyRepository.java
@@ -3,9 +3,12 @@ package com.vesmer.web.timontey.repository;
 import java.util.List;
 
 import com.vesmer.web.timontey.domain.MoneySpend;
+import com.vesmer.web.timontey.domain.MoneySpendExpense;
 
 public interface SpendMoneyRepository {
 
 	List<MoneySpend> getMoneySpendListForEmployee(long employeeId);
+	
+	List<MoneySpendExpense> getMoneyExpenseListByQuota(long quotaId);
 
 }
