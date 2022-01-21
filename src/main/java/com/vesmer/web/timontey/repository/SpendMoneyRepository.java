c01d427cfa4412263c176a96428619058ab94871
VasGorn <vas.goroshko@gmail.com>
2022-01-21 20:37:22 +0200

    feat: add method signature to get spend money expense by id in repository interface

--- src/main/java/com/vesmer/web/timontey/repository/SpendMoneyRepository.java
+++ src/main/java/com/vesmer/web/timontey/repository/SpendMoneyRepository.java
@@ -1,6 +1,7 @@
 package com.vesmer.web.timontey.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.vesmer.web.timontey.domain.MoneySpend;
 import com.vesmer.web.timontey.domain.MoneySpendExpense;
@@ -10,5 +11,7 @@ public interface SpendMoneyRepository {
 	List<MoneySpend> getMoneySpendListForEmployee(long employeeId);
 	
 	List<MoneySpendExpense> getMoneyExpenseListByQuota(long quotaId);
+
+	Optional<MoneySpendExpense> findMoneyExpenseById(long id);
 
 }
