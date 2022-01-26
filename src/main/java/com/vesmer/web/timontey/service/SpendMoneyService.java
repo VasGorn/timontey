f9cbe31bae79e31b2b75bf0d075f92ec196b6a4e
VasGorn <vas.goroshko@gmail.com>
2022-01-26 16:52:45 +0200

    feat: add method signature to update spended money on quota

--- src/main/java/com/vesmer/web/timontey/service/SpendMoneyService.java
+++ src/main/java/com/vesmer/web/timontey/service/SpendMoneyService.java
@@ -3,11 +3,14 @@ package com.vesmer.web.timontey.service;
 import java.util.List;
 
 import com.vesmer.web.timontey.domain.MoneySpend;
+import com.vesmer.web.timontey.domain.MoneySpendExpense;
 
 public interface SpendMoneyService {
 
 	List<MoneySpend> getMoneySpendListForEmployee(long employeeId);
 
 	MoneySpend save(MoneySpend moneySpend);
+
+	MoneySpendExpense update(MoneySpendExpense mSpendExpense);
 
 }
