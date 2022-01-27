c2dcd04a22f12d8ae8653c34cdaa1524a4f99336
VasGorn <vas.goroshko@gmail.com>
2022-01-27 18:12:14 +0200

    feat: add endpoint for delete request

--- src/main/java/com/vesmer/web/timontey/controllers/rest/SpendMoneyRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/SpendMoneyRest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,5 +40,12 @@ public class SpendMoneyRest {
 	public MoneySpendExpense putMoneySpendExpense(
 			@RequestBody MoneySpendExpense mSpendExpense) {
 		return spendMoneyService.update(mSpendExpense);
+	}
+		
+	@DeleteMapping("/{moneySpendExpenseId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteMoneySpendExpense(
+			@PathVariable("moneySpendExpenseId") long spendExpeseId) {
+		spendMoneyService.delete(spendExpeseId);
 	}
 }
