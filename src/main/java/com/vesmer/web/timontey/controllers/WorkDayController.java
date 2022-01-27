9136a2098a46f4e72b25afd9a3fc034df0baf87b
VasGorn <vas.goroshko@gmail.com>
2022-01-27 20:11:34 +0200

    feat: add to controller constant fields

--- src/main/java/com/vesmer/web/timontey/controllers/WorkDayController.java
+++ src/main/java/com/vesmer/web/timontey/controllers/WorkDayController.java
@@ -4,5 +4,8 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class WorkDayController {
+	private final short DAYS = 4;
+	private final short WORK_HOURS = 8;
+	private final short OVERTIME = 12;
 
 }
