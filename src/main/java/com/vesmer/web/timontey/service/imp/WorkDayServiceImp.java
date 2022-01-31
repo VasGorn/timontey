94ce98b88d8908433fec1a8c71ed221d5cf6a810
VasGorn <vas.goroshko@gmail.com>
2022-01-31 19:56:30 +0200

    feat: create work day service class that implement according service interface

---
+++ src/main/java/com/vesmer/web/timontey/service/imp/WorkDayServiceImp.java
@@ -0,0 +1,18 @@
+package com.vesmer.web.timontey.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vesmer.web.timontey.domain.HoursSpend;
+import com.vesmer.web.timontey.repository.WorkDayRepository;
+import com.vesmer.web.timontey.service.WorkDayService;
+
+
+@Service
+@Transactional
+public class WorkDayServiceImp implements WorkDayService {
+
+}
