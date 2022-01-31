7b3095d6d35387e5faec4235b7fd5532ec70224c
VasGorn <vas.goroshko@gmail.com>
2022-01-31 20:28:54 +0200

    feat: add method signature to get quota of work type hours by id

--- src/main/java/com/vesmer/web/timontey/repository/QuotaTimeRepository.java
+++ src/main/java/com/vesmer/web/timontey/repository/QuotaTimeRepository.java
@@ -1,8 +1,10 @@
 package com.vesmer.web.timontey.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.vesmer.web.timontey.domain.QuotaTime;
+import com.vesmer.web.timontey.domain.WorkTypeHours;
 
 public interface QuotaTimeRepository {
 
@@ -16,5 +18,7 @@ public interface QuotaTimeRepository {
 	void delete(long workHoursId);
 
 	List<QuotaTime> getQuotaTimeListForEmployee(long employeeId, short numMonth, short year);
+
+	Optional<WorkTypeHours> findWorkTypeQuotaById(long id);
 
 }
