e592a25ec006f9dd99cbcc6d21888b7915bf7150
VasGorn <vas.goroshko@gmail.com>
2022-01-31 19:47:39 +0200

    feat: add method signature to get list of spended hours on work type by master, month, year

--- src/main/java/com/vesmer/web/timontey/service/WorkDayService.java
+++ src/main/java/com/vesmer/web/timontey/service/WorkDayService.java
@@ -1,5 +1,11 @@
 package com.vesmer.web.timontey.service;
 
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.HoursSpend;
+
 public interface WorkDayService {
+
+	List<HoursSpend> getWorkTypeTimeSpend(long masterId, short numMonth, short year);
 
 }
