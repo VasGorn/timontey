92fc13f0f01d1ef047d772484fd2a92ff20f7cda
VasGorn <vas.goroshko@gmail.com>
2022-02-01 21:34:42 +0200

    feat: add endpoint to post spended hours on work type

--- src/main/java/com/vesmer/web/timontey/controllers/rest/WorkDayRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/WorkDayRest.java
@@ -3,9 +3,13 @@ package com.vesmer.web.timontey.controllers.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vesmer.web.timontey.domain.HoursSpend;
@@ -23,5 +27,11 @@ public class WorkDayRest {
 			@PathVariable("numMonth") short numMonth,
 			@PathVariable("year") short year){
 		return workDayService.getWorkTypeTimeSpend(masterId, numMonth, year);
+	}
+	
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public HoursSpend postHoursSpend(@RequestBody HoursSpend hoursSpend) {
+		return workDayService.save(hoursSpend);
 	}
 }
