b4e86f62b944ceecd51b430be907f09c2aa81c3e
VasGorn <vas.goroshko@gmail.com>
2022-02-01 18:02:11 +0200

    feat: add method signature to get list of spended hours on work type by master, month and year on repository interface

--- src/main/java/com/vesmer/web/timontey/repository/WorkDayRepository.java
+++ src/main/java/com/vesmer/web/timontey/repository/WorkDayRepository.java
@@ -1,5 +1,11 @@
 package com.vesmer.web.timontey.repository;
 
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.HoursSpend;
+
 public interface WorkDayRepository {
+
+	List<HoursSpend> getWorkTypeTimeSpend(long masterId, short numMonth, short year);
 
 }
