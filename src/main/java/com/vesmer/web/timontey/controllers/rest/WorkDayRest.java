780399f6c1263eb3bbba2d9eb773a69b2ba182cd
VasGorn <vas.goroshko@gmail.com>
2022-02-03 18:22:13 +0200

    feat: add endpoint for put request to update spended hours

--- src/main/java/com/vesmer/web/timontey/controllers/rest/WorkDayRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/WorkDayRest.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -33,5 +34,10 @@ public class WorkDayRest {
 	@ResponseStatus(HttpStatus.CREATED)
 	public HoursSpend postHoursSpend(@RequestBody HoursSpend hoursSpend) {
 		return workDayService.save(hoursSpend);
+	}
+		
+	@PutMapping(path = "/{workDayId}", consumes = "application/json")
+	public HoursSpend putHoursSpend(@RequestBody HoursSpend hoursSpend) {
+		return workDayService.update(hoursSpend);
 	}
 }
