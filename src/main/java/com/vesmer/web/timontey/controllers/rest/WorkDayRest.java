8c46f06441a4d7a6033c5f3c0998029c3c104ca6
VasGorn <vas.goroshko@gmail.com>
2022-02-06 18:48:52 +0200

    feat: add endpoint for delete request to remove spended hours

--- src/main/java/com/vesmer/web/timontey/controllers/rest/WorkDayRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/WorkDayRest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,5 +40,11 @@ public class WorkDayRest {
 	@PutMapping(path = "/{workDayId}", consumes = "application/json")
 	public HoursSpend putHoursSpend(@RequestBody HoursSpend hoursSpend) {
 		return workDayService.update(hoursSpend);
+	}
+		
+	@DeleteMapping("/{workDayId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteWorkDay(@PathVariable("workDayId") long workDayId) {
+		workDayService.delete(workDayId);
 	}
 }
