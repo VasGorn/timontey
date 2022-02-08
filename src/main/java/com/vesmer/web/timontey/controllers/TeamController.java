ffd95d87006aff2d675d3dfded309517e9169abe
VasGorn <vas.goroshko@gmail.com>
2022-02-08 15:57:36 +0200

    feat: create controller for team

---
+++ src/main/java/com/vesmer/web/timontey/controllers/TeamController.java
@@ -0,0 +1,15 @@
+package com.vesmer.web.timontey.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.vesmer.web.timontey.domain.Employee;
+import com.vesmer.web.timontey.service.StaffService;
+
+@Controller
+public class TeamController {
+	@Autowired
+	private StaffService staffService;
+}
