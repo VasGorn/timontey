b97ab69ed9a1defa379000244b573a3036b56865
VasGorn <vas.goroshko@gmail.com>
2022-02-09 08:28:05 +0200

    feat: define global variables in team javascript

--- src/main/webapp/static/js/team.js
+++ src/main/webapp/static/js/team.js
@@ -1,1 +1,11 @@
+var btnAdd = document.getElementById("add");
+var btnDelete = document.getElementById("delete");
 
+var hPerformerId = document.getElementById("performer_id");
+var selectEmployee = document.getElementById("select_employee");
+
+var URL_REST_TEAM = "/timontey/rest/team";
+var URL_REST_STAFF = "/timontey/rest/staff";
+
+var $table = $('#table');
+var checkedRows = [];
