0d05abc0a9d11bae66c3b2f71a0ccae46388737b
VasGorn <vas.goroshko@gmail.com>
2022-02-09 20:02:20 +0200

    feat: add endpoint for get request to find the team by leader id

--- src/main/java/com/vesmer/web/timontey/controllers/rest/TeamRest.java
+++ src/main/java/com/vesmer/web/timontey/controllers/rest/TeamRest.java
@@ -1,9 +1,12 @@
 package com.vesmer.web.timontey.controllers.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vesmer.web.timontey.domain.Team;
 import com.vesmer.web.timontey.service.TeamService;
 
 @RestController
@@ -11,5 +14,10 @@ import com.vesmer.web.timontey.service.TeamService;
 public class TeamRest {
 	@Autowired
 	private TeamService teamService;
-
+		
+	@GetMapping("/performer/{performerId}")
+	public Team getTeamByPerformerId(
+			@PathVariable("performerId") long performerId ) {
+		return teamService.getTeam(performerId);
+	}
 }
