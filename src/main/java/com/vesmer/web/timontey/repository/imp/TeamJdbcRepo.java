83d74b0e1f3ad6bf9e097415ceae7a2bf6ead61f
VasGorn <vas.goroshko@gmail.com>
2022-02-10 09:12:35 +0200

    feat: implement method to get team by leader id in repository class with jdbc implementation

---
+++ src/main/java/com/vesmer/web/timontey/repository/imp/TeamJdbcRepo.java
@@ -0,0 +1,40 @@
+package com.vesmer.web.timontey.repository.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.vesmer.web.timontey.domain.Employee;
+import com.vesmer.web.timontey.domain.Team;
+import com.vesmer.web.timontey.repository.StaffRepository;
+import com.vesmer.web.timontey.repository.TeamRepository;
+import com.vesmer.web.timontey.rowmapper.EmployeeRowMapper;
+
+@Repository
+public class TeamJdbcRepo implements TeamRepository {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+		
+	@Autowired
+	private StaffRepository staffRepository;
+	
+	private static final String SELECT_EMPLOYEES_BY_MASTER_ID_SQL = 
+		"SELECT s.id, s.last_name, s.first_name, s.middle_name "
+		+ "FROM staff AS s, master_employees AS me "
+		+ "WHERE s.id = me.employee_id AND me.master_id = ?;";
+
+	@Override
+	public Team getTeam(long performerId) {
+		List<Employee> teamList = 
+				(List<Employee>) jdbcTemplate.query(SELECT_EMPLOYEES_BY_MASTER_ID_SQL,
+				new EmployeeRowMapper(), performerId);
+		Employee performer = staffRepository.findById(performerId).get();
+
+		Team team = new Team();
+		team.setPerformer(performer);
+		team.setEmployeeList(teamList);
+		return team;
+	}
+}
