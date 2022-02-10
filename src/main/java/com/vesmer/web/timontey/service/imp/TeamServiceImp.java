441c46fd2189fb2c5bf2d673a6be077d12161a37
VasGorn <vas.goroshko@gmail.com>
2022-02-10 08:23:24 +0200

    feat: implement method to get team by leader id in service class

---
+++ src/main/java/com/vesmer/web/timontey/service/imp/TeamServiceImp.java
@@ -0,0 +1,20 @@
+package com.vesmer.web.timontey.service.imp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.vesmer.web.timontey.domain.Team;
+import com.vesmer.web.timontey.repository.TeamRepository;
+import com.vesmer.web.timontey.service.TeamService;
+
+@Service
+public class TeamServiceImp implements TeamService {
+	@Autowired
+	private TeamRepository teamRepository;
+
+	@Override
+	public Team getTeam(long performerId) {
+		return teamRepository.getTeam(performerId);
+	}
+}
