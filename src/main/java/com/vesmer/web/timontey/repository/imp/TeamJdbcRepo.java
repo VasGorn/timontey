546c687ad5e7c28449423d283aaca843d8993ae8
VasGorn <vas.goroshko@gmail.com>
2022-02-10 11:19:42 +0200

    feat: implement method to add employee to team in repository class with jdbc implementation

--- src/main/java/com/vesmer/web/timontey/repository/imp/TeamJdbcRepo.java
+++ src/main/java/com/vesmer/web/timontey/repository/imp/TeamJdbcRepo.java
@@ -24,9 +24,27 @@ public class TeamJdbcRepo implements TeamRepository {
 		"SELECT s.id, s.last_name, s.first_name, s.middle_name "
 		+ "FROM staff AS s, master_employees AS me "
 		+ "WHERE s.id = me.employee_id AND me.master_id = ?;";
+	private static final String INSERT_EMPLOYEE_TO_PERFORMER_SQL = 
+		"INSERT INTO master_employees (master_id, employee_id) "
+		+ "VALUES (?, ?);";
 
 	@Override
 	public Team getTeam(long performerId) {
+		List<Employee> teamList = 
+				(List<Employee>) jdbcTemplate.query(SELECT_EMPLOYEES_BY_MASTER_ID_SQL,
+				new EmployeeRowMapper(), performerId);
+		Employee performer = staffRepository.findById(performerId).get();
+
+		Team team = new Team();
+		team.setPerformer(performer);
+		team.setEmployeeList(teamList);
+		return team;
+	}
+	
+	@Override
+	public Team saveEmployeeToPerformer(long performerId, long employeeId) {
+		jdbcTemplate.update(INSERT_EMPLOYEE_TO_PERFORMER_SQL, performerId, employeeId);
+
 		List<Employee> teamList = 
 				(List<Employee>) jdbcTemplate.query(SELECT_EMPLOYEES_BY_MASTER_ID_SQL,
 				new EmployeeRowMapper(), performerId);
