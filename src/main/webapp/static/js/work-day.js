c14ceecaffee617b29dd9f21b50fdfc3c9887ecd
VasGorn <vas.goroshko@gmail.com>
2022-02-11 10:16:03 +0200

    feat: remove options from select form element

--- src/main/webapp/static/js/work-day.js
+++ src/main/webapp/static/js/work-day.js
@@ -465,3 +465,12 @@ function setOrderInfo(order){
 	txtDescription.value = order.description;
 	txtAddress.value = order.address;
 }
+
+function removeOptionsFromSelect(select){
+	const length = select.options.length;
+	select.value = 0;
+	
+	for(let i = 1; i < length; ++i){
+		select.removeChild(select.options[1]);
+	}
+}
