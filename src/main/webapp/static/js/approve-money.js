40ef498d49b057bac4eb4fda327011e1df5909c3
VasGorn <vas.goroshko@gmail.com>
2022-02-12 19:25:50 +0200

    feat: define global variables in approve money javascript

--- src/main/webapp/static/js/approve-money.js
+++ src/main/webapp/static/js/approve-money.js
@@ -1,1 +1,26 @@
+var btnApprove = document.getElementById("approve");
+var btnUpdate = document.getElementById("update");
+var btnDelete = document.getElementById("delete");
 
+var hManagerId = document.getElementById("manager_id");
+
+var selectOrder = document.getElementById("select_order");
+var txtDescription = document.getElementById("description");
+var txtAddress = document.getElementById("address");
+
+var numOrderBalanceMoney = document.getElementById("order_balance_money");
+var numOrderLimitMoney = document.getElementById("order_limit_money");
+
+var selectExpenses = document.getElementById("select_expenses");
+var numMoneySpend = document.getElementById("money_spend");
+
+var $table = $('#table');
+
+var URL_REST_SPEND_MONEY = "/timontey/rest/spend-money";
+var URL_REST_EXPENSES = "/timontey/rest/expenses";
+
+var ORDER_MONEY_ARRAY = [];
+var LIMIT_MONEY = 0.0;
+var BALANCE_MONEY = 0.0;
+
+var checkedRows = [];
