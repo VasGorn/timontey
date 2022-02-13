cadbce5c1b6e32a4dee97d7d33a412cb935c4ea2
VasGorn <vas.goroshko@gmail.com>
2022-02-13 20:28:06 +0200

    feat: find index in table for selected row object

--- src/main/webapp/static/js/approve-money.js
+++ src/main/webapp/static/js/approve-money.js
@@ -256,3 +256,16 @@ function getDataFromForm(employeeId){
 	
 	return moneySpend;
 }
+
+function findIndexInTable(row) {
+	let tableData = $table.bootstrapTable('getData');
+	let index = 0;
+
+	for (let i = 0; i < tableData.length; ++i) {
+		if (row.id === tableData[i].id) {
+			index = i;
+			break;
+		}
+	}
+	return index;
+}
