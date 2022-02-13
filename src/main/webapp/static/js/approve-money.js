e9fe42351de6957ae9946e7e0a0d8b7d639fc8d7
VasGorn <vas.goroshko@gmail.com>
2022-02-13 17:58:14 +0200

    feat: set disable state of buttons based on length of checked rows

--- src/main/webapp/static/js/approve-money.js
+++ src/main/webapp/static/js/approve-money.js
@@ -179,3 +179,23 @@ function deleteCheckedRow(spendId, checkedRows){
 		}
 	}
 }
+
+function setButtonsState(numRowSelected) {
+	if (numRowSelected < 1) {
+		btnApprove.disabled = true;
+		btnUpdate.disabled = true;
+		btnDelete.disabled = true;
+		return;
+	}
+	if (numRowSelected === 1) {
+		btnApprove.disabled = false;
+		btnUpdate.disabled = false;
+		btnDelete.disabled = false;
+		return;
+	}
+	if (numRowSelected > 1) {
+		btnApprove.disabled = false;
+		btnUpdate.disabled = true;
+		btnDelete.disabled = false;
+	}
+}
