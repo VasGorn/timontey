bd2e1aad8adeda78f512c823117a49a29d8c60fe
VasGorn <vas.goroshko@gmail.com>
2022-02-14 15:19:59 +0200

    feat: define global variables in approve time javascript

--- src/main/webapp/static/js/approve-time.js
+++ src/main/webapp/static/js/approve-time.js
@@ -1,1 +1,33 @@
+var btnApprove = document.getElementById("approve");
+var btnUpdate = document.getElementById("update");
+var btnDelete = document.getElementById("delete");
 
+var hManagerId = document.getElementById("manager_id");
+var hYear = document.getElementById("year");
+var hNumMonth = document.getElementById("num_month");
+
+var selectOrder = document.getElementById("select_order");
+var txtDescription = document.getElementById("description");
+var txtAddress = document.getElementById("address");
+
+var numOrderBalanceTime = document.getElementById("order_balance_hours");
+var numOrderLimitTime = document.getElementById("order_limit_hours");
+
+var selectEmployee = document.getElementById("select_employee");
+var numWorkTypeBalanceTime = document.getElementById("balance_hours");
+var numWorkTypeLimitTime = document.getElementById("limit_hours");
+
+var selectDayOfMonth = document.getElementById("select_day_of_month");
+var selectWorkType = document.getElementById("select_worktype");
+var selectWorkHour = document.getElementById("select_work_hours");
+var selectOvertime = document.getElementById("select_overtime");
+
+var $table = $('#table');
+
+var URL_REST_QUOTA_TIME = "/timontey/rest/quota-time";
+var URL_REST_STAFF = "/timontey/rest/staff";
+var URL_REST_WORK_DAY = "/timontey/rest/work-day";
+
+var QUOTA_TIME_ARRAY = [];
+
+var checkedRows = [];
