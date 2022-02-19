0b2fa476db3b0c76abaaf95dccd053ce144e75c9
VasGorn <vas.goroshko@gmail.com>
2022-02-19 10:51:37 +0200

    feat: create authority class

---
+++ src/main/java/com/vesmer/web/timontey/domain/Authority.java
@@ -0,0 +1,50 @@
+package com.vesmer.web.timontey.domain;
+
+import java.util.Objects;
+
+public class Authority {
+	private long id;
+	private String name;
+	
+	public Authority() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Authority [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Authority other = (Authority) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+
+}
