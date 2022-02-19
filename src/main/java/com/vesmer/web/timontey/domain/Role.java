e8be3223a4404c4a73ccc75eefbd073a6bd33f88
VasGorn <vas.goroshko@gmail.com>
2022-02-19 10:58:42 +0200

    feat: add new field of list authorities to role class

--- src/main/java/com/vesmer/web/timontey/domain/Role.java
+++ src/main/java/com/vesmer/web/timontey/domain/Role.java
@@ -1,8 +1,11 @@
 package com.vesmer.web.timontey.domain;
 
+import java.util.List;
+
 public class Role {
 	private long id;
 	private String roleName;
+	private List<Authority> authorities;
 	
 	public Role() {
 	}
@@ -19,10 +22,15 @@ public class Role {
 	public void setRoleName(String roleName) {
 		this.roleName = roleName;
 	}
+	public List<Authority> getAuthorities() {
+		return authorities;
+	}
+	public void setAuthorities(List<Authority> authorities) {
+		this.authorities = authorities;
+	}
 
 	@Override
 	public String toString() {
 		return "Role [id=" + id + ", roleName=" + roleName + "]";
 	}
-	
 }
