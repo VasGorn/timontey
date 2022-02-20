88edc7ec51ba536f1e837b5e203dca6b36ee55be
VasGorn <vas.goroshko@gmail.com>
2022-02-20 09:32:27 +0200

    feat: implement method to get list of navigation links by role in repository class with JDBC realization

--- src/main/java/com/vesmer/web/timontey/repository/imp/NavigationLinkJdbcRepo.java
+++ src/main/java/com/vesmer/web/timontey/repository/imp/NavigationLinkJdbcRepo.java
@@ -9,10 +9,22 @@ import org.springframework.stereotype.Repository;
 import com.vesmer.web.timontey.domain.NavigationLink;
 import com.vesmer.web.timontey.domain.Role;
 import com.vesmer.web.timontey.repository.NavigationLinkRepository;
+import com.vesmer.web.timontey.rowmapper.NavigationLinkRowMapper;
 
 @Repository
 public class NavigationLinkJdbcRepo implements NavigationLinkRepository{
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+		
+	private static final String SELECT_NAVIGATION_BY_ROLE_SQL = 
+		"SELECT n.id, n.name, n.url, n.icon "
+		+ "FROM navigation_link AS n, role_navigation AS rn, roles AS r "
+		+ "WHERE n.id = rn.navigation_id AND r.id = rn.role_id AND r.id = ? "
+		+ "ORDER BY n.name;";
 
+	@Override
+	public List<NavigationLink> getNavigationByRole(Role role) {
+		return jdbcTemplate.query(SELECT_NAVIGATION_BY_ROLE_SQL, 
+				new NavigationLinkRowMapper(), role.getId());
+	}
 }
