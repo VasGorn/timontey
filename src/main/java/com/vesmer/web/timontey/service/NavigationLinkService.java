9807113f06b6c9c0aac460a27d63518f26be04a9
VasGorn <vas.goroshko@gmail.com>
2022-02-20 08:03:18 +0200

    feat: add method signature to get list of navigation links by role to service interface

--- src/main/java/com/vesmer/web/timontey/service/NavigationLinkService.java
+++ src/main/java/com/vesmer/web/timontey/service/NavigationLinkService.java
@@ -1,5 +1,12 @@
 package com.vesmer.web.timontey.service;
 
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.NavigationLink;
+import com.vesmer.web.timontey.domain.Role;
+
 public interface NavigationLinkService {
+
+	List<NavigationLink> getNavigationByRole(Role role);
 
 }
