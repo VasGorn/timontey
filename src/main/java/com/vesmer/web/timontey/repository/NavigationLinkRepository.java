251037f6e3cf107ac0b3d90a281c66a2f4294a14
VasGorn <vas.goroshko@gmail.com>
2022-02-20 08:40:55 +0200

    feat: add method signature to get list of navigation links by role to repository interface

--- src/main/java/com/vesmer/web/timontey/repository/NavigationLinkRepository.java
+++ src/main/java/com/vesmer/web/timontey/repository/NavigationLinkRepository.java
@@ -1,5 +1,12 @@
 package com.vesmer.web.timontey.repository;
 
+import java.util.List;
+
+import com.vesmer.web.timontey.domain.NavigationLink;
+import com.vesmer.web.timontey.domain.Role;
+
 public interface NavigationLinkRepository {
+
+	List<NavigationLink> getNavigationByRole(Role role);
 
 }
