df8c4ac176fe38a3712351e295ad34fec1f8f6be
VasGorn <vas.goroshko@gmail.com>
2022-04-09 16:52:20 +0300

    feat: add endpoint to get request for the report money view

--- src/main/java/com/vesmer/web/timontey/controllers/ReportMoneyController.java
+++ src/main/java/com/vesmer/web/timontey/controllers/ReportMoneyController.java
@@ -1,9 +1,14 @@
 package com.vesmer.web.timontey.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.vesmer.web.timontey.domain.Employee;
 import com.vesmer.web.timontey.service.ReportMoneyService;
 import com.vesmer.web.timontey.service.StaffService;
 
@@ -15,4 +20,22 @@ public class ReportMoneyController {
 	
 	@Autowired
 	private StaffService staffService;
+
+	@RequestMapping("")
+	public ModelAndView getReportMoneyView() {
+		long managerId = 3;
+		Employee manager = staffService.getEmployeeById(managerId).get();
+		String fullName = manager.getLastName() + " " + manager.getFirstName() 
+							+ " " + manager.getMiddleName();
+		
+		List<String> years = getYearsList();
+		HashMap<Integer, String> months = getMonthsMap();
+		
+		ModelAndView model = new ModelAndView("report-money");
+		model.addObject("managerId", manager.getId());
+		model.addObject("managerName", fullName);
+		model.addObject("years", years);
+		model.addObject("months", months);
+		return model;
+	}
 }
