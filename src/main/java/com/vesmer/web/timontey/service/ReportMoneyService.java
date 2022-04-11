69085eca26627c31d057622c3181c76c52beaf88
VasGorn <vas.goroshko@gmail.com>
2022-04-11 17:20:49 +0300

    feat: add method signature to get money report in service interface

--- src/main/java/com/vesmer/web/timontey/service/ReportMoneyService.java
+++ src/main/java/com/vesmer/web/timontey/service/ReportMoneyService.java
@@ -1,5 +1,9 @@
 package com.vesmer.web.timontey.service;
 
+import com.vesmer.web.timontey.domain.ReportMoney;
+
 public interface ReportMoneyService {
+
+	ReportMoney getReportMoney(long orderId, short year, short numMonth);
 
 }
