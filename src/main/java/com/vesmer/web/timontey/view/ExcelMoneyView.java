80714a156c4dc4e9c270f07d8745fa62c542f47d
VasGorn <vas.goroshko@gmail.com>
2022-04-24 17:53:22 +0300

    feat: create the class to build excel file for money report

---
+++ src/main/java/com/vesmer/web/timontey/view/ExcelMoneyView.java
@@ -0,0 +1,20 @@
+package com.vesmer.web.timontey.view;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.servlet.view.document.AbstractXlsView;
+
+public class ExcelMoneyView extends AbstractXlsView {
+
+	@Override
+	protected void buildExcelDocument(Map<String, Object> model, 
+			Workbook workbook, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+	}
+
+}
