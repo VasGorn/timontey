bb2c65d942126a2581490a02371cc9e0dd13c808
VasGorn <vas.goroshko@gmail.com>
2022-05-06 10:48:02 +0300

    feat: add fields and constructor in the untility class for cell style

--- src/main/java/com/vesmer/web/timontey/utilities/ExcelStyleCell.java
+++ src/main/java/com/vesmer/web/timontey/utilities/ExcelStyleCell.java
@@ -1,5 +1,20 @@
 package com.vesmer.web.timontey.utilities;
 
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Workbook;
+
 public class ExcelStyleCell {
+	private Workbook workbook;
+	private CellStyle main;
+	private CellStyle headerGreen;
+	private CellStyle headerBlackCenter;
+	private CellStyle blackCenter;
+	private CellStyle blackBoldLeft;
+	private CellStyle fillGreen;
+	private CellStyle fillGreenBold;
+	
+	public ExcelStyleCell(Workbook workbook) {
+		this.workbook = workbook;
+	}
 
 }
