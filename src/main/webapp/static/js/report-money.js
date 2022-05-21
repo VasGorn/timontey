a26088ff4698887ed99686155c2694f76d492e56
VasGorn <vas.goroshko@gmail.com>
2022-05-21 20:53:25 +0300

    feat: define and initialize global variables in javascript file of the report money page

--- src/main/webapp/static/js/report-money.js
+++ src/main/webapp/static/js/report-money.js
@@ -1,3 +1,18 @@
-/**
- * 
- */
+var btnDownload = document.getElementById("create_report");
+var formReportTime = document.getElementById("form_report_money");
+
+var selectOrder = document.getElementById("select_order");
+var hiddenManagerId = document.getElementById("manager_id");
+var txtDescription = document.getElementById("description");
+var txtAddress = document.getElementById("address");
+
+var selectYear = document.getElementById("select_year");
+var selectMonth = document.getElementById("select_month");
+
+var txtFilename = document.getElementById("filename");
+
+var btnCreateReport = document.getElementById("btn_create_report");
+
+var ORDERS = [];
+
+var URL_REST_ORDER = "/timontey/rest/orders";
