9925951cdafea1984fe0e070f2213df948edbb8c
VasGorn <vas.goroshko@gmail.com>
2022-06-02 21:40:37 +0300

    refactor: create class for base implementation of the money report builder for excel

---
+++ src/main/java/com/vesmer/web/timontey/reports/money/excel/implementation/ExcelMoneyReportBaseImpl.java
@@ -0,0 +1,10 @@
+package com.vesmer.web.timontey.reports.money.excel.implementation;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+import com.vesmer.web.timontey.domain.ReportMoney;
+import com.vesmer.web.timontey.reports.money.excel.ExcelMoneyReport;
+
+public class ExcelMoneyReportBaseImpl implements ExcelMoneyReport {
+
+}
