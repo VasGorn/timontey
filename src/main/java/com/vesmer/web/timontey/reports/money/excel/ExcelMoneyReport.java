2291044dd6795401f9d8d8ebe90355af10f48114
VasGorn <vas.goroshko@gmail.com>
2022-06-04 17:47:35 +0300

    refactor: change method signature to build excel money report without argument

--- src/main/java/com/vesmer/web/timontey/reports/money/excel/ExcelMoneyReport.java
+++ src/main/java/com/vesmer/web/timontey/reports/money/excel/ExcelMoneyReport.java
@@ -2,8 +2,6 @@ package com.vesmer.web.timontey.reports.money.excel;
 
 import org.apache.poi.ss.usermodel.Workbook;
 
-import com.vesmer.web.timontey.domain.ReportMoney;
-
 public interface ExcelMoneyReport {
-	Workbook excelReport(ReportMoney reportMoney);
+	Workbook excelReport();
 }
