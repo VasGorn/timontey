39087ea9f442e1e9290445a07522785c413a9dd6
VasGorn <vas.goroshko@gmail.com>
2022-06-05 19:21:55 +0300

    refactor: incapsulate in base implementation of excel order information the quota of money

--- src/main/java/com/vesmer/web/timontey/reports/money/excel/implementation/ExcelOrderInformationBaseImpl.java
+++ src/main/java/com/vesmer/web/timontey/reports/money/excel/implementation/ExcelOrderInformationBaseImpl.java
@@ -1,5 +1,6 @@
 package com.vesmer.web.timontey.reports.money.excel.implementation;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -8,38 +9,47 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
+import com.vesmer.web.timontey.domain.Order;
+import com.vesmer.web.timontey.domain.QuotaMoney;
 import com.vesmer.web.timontey.reports.money.excel.ExcelOrderInformation;
 import com.vesmer.web.timontey.reports.money.excel.style.DefaultCellStyle;
 import com.vesmer.web.timontey.reports.money.excel.style.ExcelCellStyle;
 import com.vesmer.web.timontey.reports.money.excel.style.HeaderGreenCellStyle;
 
-public final class ExcelOrderInformationBaseImpl implements ExcelOrderInformation{
+public final class ExcelOrderInformationBaseImpl implements ExcelOrderInformation {
+	private final QuotaMoney quotaMoney;
 	private final ExcelCellStyle headerStyle;
 	private final ExcelCellStyle defaultStyle;
 	private final int rowStartIndex;
 	private final int columnStartIndex;
 	private final int maxColumnWidth;
-	
-	public ExcelOrderInformationBaseImpl(Workbook workbook) {
-		this(new HeaderGreenCellStyle(workbook), new DefaultCellStyle(workbook), 
-				1, 0, 5000);
+
+	public ExcelOrderInformationBaseImpl(Workbook workbook, QuotaMoney quotaMoney) {
+		this(quotaMoney, 
+				new HeaderGreenCellStyle(workbook), 
+				new DefaultCellStyle(workbook), 
+				1, 
+				0, 
+				5000);
 	}
 
-	public ExcelOrderInformationBaseImpl(ExcelCellStyle headerStyle, 
-			ExcelCellStyle defaultStyle, int rowStartIndex, 
-			int columnStartIndex, int maxColumntWith) {
+	public ExcelOrderInformationBaseImpl(QuotaMoney quotaMoney, 
+			ExcelCellStyle headerStyle, ExcelCellStyle defaultStyle,
+			int rowStartIndex, int columnStartIndex, int maxColumnWidth) {
+		this.quotaMoney = quotaMoney;
 		this.headerStyle = headerStyle;
 		this.defaultStyle = defaultStyle;
 		this.rowStartIndex = rowStartIndex;
 		this.columnStartIndex = columnStartIndex;
-		this.maxColumnWidth = maxColumntWith;
+		this.maxColumnWidth = maxColumnWidth;
 	}
 
 	@Override
-	public void writeOrderInformation(Sheet sheet, Map<String, String> headerMap) {
-       	sheet.setColumnWidth(columnStartIndex, maxColumnWidth);
+	public void writeOrderInformation(Sheet sheet) {
+		sheet.setColumnWidth(columnStartIndex, maxColumnWidth);
+		Map<String, String> headerMap = headerMap();
 		int i = 1;
-		for(Map.Entry<String, String> element: headerMap.entrySet()) {
+		for (Map.Entry<String, String> element : headerMap.entrySet()) {
 			Row row = sheet.createRow(rowStartIndex + i);
 			writeToCell(row, columnStartIndex, element.getKey(), headerStyle.cellStyle());
 			writeToCell(row, columnStartIndex + 1, element.getValue(), defaultStyle.cellStyle());
@@ -47,10 +57,19 @@ public final class ExcelOrderInformationBaseImpl implements ExcelOrderInformatio
 		}
 	}
 
-	private void writeToCell(Row row, int numColumn, String value, 
-			CellStyle style) {
+	private void writeToCell(Row row, int numColumn, String value, CellStyle style) {
 		Cell cell = row.createCell(numColumn);
 		cell.setCellStyle(style);
 		cell.setCellValue(value);
+	}
+
+	private Map<String, String> headerMap() {
+		Map<String, String> headerMap = new LinkedHashMap<String, String>(4);
+		Order order = quotaMoney.getOrder();
+		headerMap.put("ORDER", order.toString());
+		headerMap.put("ADDRESS", order.getAddress());
+		headerMap.put("DESCRIPTION", order.getDescription());
+		headerMap.put("PERFORMER", quotaMoney.getEmployee().toString());
+		return headerMap;
 	}
 }
